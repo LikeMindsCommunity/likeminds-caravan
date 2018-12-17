5e7a88da5696ee1894b6279e10340f149b2688c9
apoorv <apoorvkhare007@gmail.com>
2018-12-17 17:23:59 +0530

    api integration

--- project/togther/forms.py
+++ project/togther/forms.py
@@ -3,7 +3,16 @@ from .models import *
 from categories  import Category_list
 
 class NewGroupForm(forms.ModelForm):
-    category = forms.CharField(label='Category', widget=forms.Select(choices=Category_list))
+    # category = forms.CharField(label='Category', widget=forms.Select(choices=Category_list))
+    category = forms.MultipleChoiceField(
+            choices=Category_list,
+            initial='0',
+            widget=forms.SelectMultiple(attrs={
+            'placeholder': 'Additional tags',
+            'class': 'ui search fluid dropdown'}),
+            required=True,
+            label='Category',
+        )
     class Meta:
         model = Community
         fields = ['name','purpose','about','category', 'location', 'image_url', 'whatsapp_group_link']
