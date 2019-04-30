ddb9450f47e02bcf15a9b47f98e49dd534ca063c
Apoorv Khare <apoorvkhare007@gmail.com>
2019-05-01 00:47:55 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'togther',
-        'USER': 'collabmates',
+        'USER': 'apoorv',
         'PASSWORD': 'khare',
         'HOST': '127.0.0.1',
         'PORT': '5432',
