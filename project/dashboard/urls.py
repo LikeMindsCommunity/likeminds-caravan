256917d699fd3cd9b9f5e177112cd659c98a0175
mahesh <mahesh61437mahe@gmail.com>
2019-06-26 15:36:46 +0530

    fix/admin dashboard url

--- project/dashboard/urls.py
+++ project/dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', dashboard, name="dashboard"),
+    path('', dashboard, name="admindashboard"),
     path('update_form/<int:community_id>', update_form, name='update_form'),
     path('community_delete/<int:community_id>',community_delete,name='community_delete'),
     path('add_dashboard_admin/<int:community_id>',add_dashboard_admin,name='add_dashboard_admin')
