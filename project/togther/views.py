a9f749911bdd2ff914393db23d00f0951429d891
mahesh <mahesh61437mahe@gmail.com>
2019-06-30 20:04:24 +0530

    fix cta dependency is removed from accept admin

--- project/togther/views.py
+++ project/togther/views.py
@@ -261,19 +261,22 @@ def accept_admin(request,community_id,cta=''):
     nom_admin = Userinfo.objects.all().filter(user_id = core_user.id)
     print("nom_admin == ",nom_admin[0].name)
     cur_sess_user_id = core_user.id
-    if cta =='':
-        cta = check_admins(community_id)
-    if cta == 'accept_invitation_admin':
-        if len(member) == 1:
+
+    if len(member) == 1:
+        if member[0].state == 1:
             print(nom_admin[0].name)
             print("email to proposed admin for single admin")
             send_email_to_proposed_admin.delay(NominatedAdmin=nom_admin[0].name,email=prop_admin.email,ProposedAdmin=prop_admin.name,CommunityName=community.name)
-        Members.objects.filter(community_id = community,member_id=core_user.id).update(state =1)
-    elif cta == 'accept_invitation_temp_admin':
-        temp_admin = Members.objects.filter(community_id = community,state=2)
-        Members.objects.filter(community_id = community,member_id=temp_admin[0].member_id).update(state =4)
-        Members.objects.filter(community_id = community,member_id=core_user.id).update(state =1)
+            Members.objects.filter(community_id = community,member_id=core_user.id).update(state =1)
+        if member[0].state == 2:
+            print("email to temp admin")
+            temp_admin = Members.objects.filter(community_id = community,state=2)
+            Members.objects.filter(community_id = community,member_id=temp_admin[0].member_id).update(state =4)
+            Members.objects.filter(community_id = community,member_id=core_user.id).update(state =1)
+            send_email_to_proposed_admin.delay(NominatedAdmin=nom_admin[0].name,email=prop_admin.email,ProposedAdmin=prop_admin.name,CommunityName=community.name)
+    else:
         send_email_to_proposed_admin.delay(NominatedAdmin=nom_admin[0].name,email=prop_admin.email,ProposedAdmin=prop_admin.name,CommunityName=community.name)
+        Members.objects.filter(community_id = community,member_id=core_user.id).update(state =1)
     return HttpResponseRedirect(reverse('comunity', args=[community_id]))
 
 def check_admins(community_id):
