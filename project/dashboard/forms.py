7526d53c4f43fb9cee094d7fb7da7a7d47e591c0
apple <apple@apples-MacBook-Pro.local>
2019-07-02 18:20:53 +0530

    adding login type in dashboard

--- project/dashboard/forms.py
+++ project/dashboard/forms.py
@@ -54,7 +54,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model=Userinfo
-        fields=['name','city','contact_number','interests','fb_link','linkedin_link','fcm_token']
+        fields=['name','city','contact_number','interests','fb_link','linkedin_link','fcm_token','login_type']
         attrs = {'class': 'form-control form-group'}
 
         widgets = {
@@ -64,7 +64,8 @@ class UserForm(forms.ModelForm):
             'interests':forms.TextInput(attrs=attrs),
             'fb_link':forms.TextInput(attrs=attrs),
             'linkedin_link':forms.TextInput(attrs=attrs),
-            'fcm_token':forms.TextInput(attrs=attrs)
+            'fcm_token':forms.TextInput(attrs=attrs),
+            'login_type':forms.TextInput(attrs=attrs)
 
         }
 class SendNominatedEmail(forms.Form):
