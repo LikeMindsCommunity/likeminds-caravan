777aed55f778b487183f511e654a65e35c1faa3c
Collabmates_2017 <collabmates_2017@Collabmates-2017s-MacBook-Pro.local>
2019-07-04 18:53:12 +0530

    mail text changes

--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -330,7 +330,7 @@ def send_email_to_nominated_admin(NominatedAdmin,email,ProposedAdmin,CommunityNa
     subject =str(ProposedAdmin)+ " has proposed you as promoter of "+str(CommunityName)+" community"
     if proposedAdminState == 1:
         template = get_template("mails/accept_admin_request.html").render({"NominatedAdmin":NominatedAdmin,"email":email,"ProposedAdmin":ProposedAdmin,"CommunityName":CommunityName,"community_id":community_id,'url':url})
-    if proposedAdminState == 2:
+    elif proposedAdminState == 2:
         template = get_template("mails/accept_temp_admin_request.html").render({"NominatedAdmin":NominatedAdmin,"email":email,"ProposedAdmin":ProposedAdmin,"CommunityName":CommunityName,"community_id":community_id,'url':url})
     msg = EmailMultiAlternatives(subject,
                                      template,
