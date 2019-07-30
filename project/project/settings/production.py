b85451433bb97a7f71ef2b65cead31f4c262154c
apple <apple@apples-MacBook-Pro.local>
2019-07-30 11:59:13 +0530

    changing firebase field for development and production

--- project/project/settings/production.py
+++ project/project/settings/production.py
@@ -19,3 +19,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+FIREBASE_CONFIG = {
+    'apiKey': "AIzaSyCmu_u-n31x2WMQlWAciP5RDXGn2qMuXrg",
+    'authDomain': "collabmates-3d601.firebaseapp.com",
+    'databaseURL': "https://collabmates-3d601.firebaseio.com",
+    'projectId': "collabmates-3d601",
+    'storageBucket': "collabmates-3d601.appspot.com",
+    'messagingSenderId': "645716458793",
+    'appId': "1:645716458793:web:779debf3286d6049"
+  };
