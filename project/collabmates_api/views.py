21c9cbcf09462d1000b3fc96710df7645423c309
MaheshRoyal <mahesh61437mahe@gmail.com>
2019-08-01 15:20:56 +0530

    sending notification params seperately

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -1326,7 +1326,7 @@ def accept_invitation(request):
                 send_email_to_proposed_admin.delay(NominatedAdmin=nom_admin[0].name,email=prop_admin.email,ProposedAdmin=prop_admin.name,proposedAdminState =1,CommunityName=community.name,community_id = community.id)
                 proposer_id = prop_admin.user_id.id
                 nom_admin_name = nom_admin[0].name
-                send_notification_to_proposer.delay(proposer_id, community, nom_admin_name)
+                send_notification_to_proposer.delay(proposer_id,community_name =community.name,community_id=community.id,proposed_name = nom_admin_name)
                 return JsonResponse({'success':True})
             # if the promoter is a temporary promoter
             elif promoter[0].state == 2:
@@ -1341,7 +1341,7 @@ def accept_invitation(request):
                 send_email_to_proposed_admin.delay(NominatedAdmin=nom_admin[0].name,email=prop_admin.email,ProposedAdmin=prop_admin.name,proposedAdminState=2,CommunityName=community.name,community_id = community.id)
                 proposer_id = prop_admin.user_id.id
                 nom_admin_name = nom_admin[0].name
-                send_notification_to_proposer.delay(proposer_id, community_name =community.name,community_id=community.id, nom_admin_name)
+                send_notification_to_proposer.delay(proposer_id, community_name =community.name,community_id=community.id,proposed_name = nom_admin_name)
                 return JsonResponse({'success':True})
         else:
             # if there are more than two admins , sent mail to the promoter who invited this member
@@ -1359,7 +1359,7 @@ def accept_invitation(request):
             send_email_to_proposed_admin.delay(NominatedAdmin=nom_admin[0].name,email=prop_admin.email,ProposedAdmin=prop_admin.name,proposedAdminState=1,CommunityName=community.name,community_id = community.id)
             proposer_id=prop_admin.user_id.id
             nom_admin_name=nom_admin[0].name
-            send_notification_to_proposer.delay(proposer_id, community,nom_admin_name)
+            send_notification_to_proposer.delay(proposer_id, community_name =community.name,community_id=community.id,proposed_name = nom_admin_name)
             return JsonResponse({'success':True})
     else:
         # if nominated promoter didn't accept the invitation
