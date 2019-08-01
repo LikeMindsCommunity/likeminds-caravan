d530946fdfea1738f8bf8441ab76780cda19b43c
apple <apple@apples-MacBook-Pro.local>
2019-08-01 14:25:33 +0530

    fixing multiple response on questions

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -348,8 +348,8 @@ def join_community_responses(request):
             response.community = community.id
             response.save()
     Community.objects.filter(id=community_id).update(updated_at=time.time())
-
-    send_notification_to_admins.delay(community_id,userinfo)
+    name=userinfo.name
+    send_notification_to_admins.delay(community_id,name)
     return JsonResponse({'success':True})
 
 
@@ -449,6 +449,7 @@ def create_community(request):
                 elif dict['key'] == 'Type of community':
                     categories = dict['value']
                     categories = categories.split(", ")
+                    group.save()
                     for tags in categories:
                         tags_id = int(tags)
                         tags_object = Tags.objects.get(id=tags_id)
@@ -633,9 +634,6 @@ def send_email_for_collabcard(community,user,card):
             context['to']=userinfo.email
             #print(context)
             send_email_for_new_collabcard_posted.delay(context)
-
-
-
 
 
 
@@ -1173,6 +1171,7 @@ def request_response(request):
         Members.objects.filter(member_id=member_id,community_id=community).update(state=5)  # decline state = 5
         # and also send notification
         send_notification_for_join_requests.delay(community_id, False, member_id)
+        Form_response.objects.filter(user=member_id,community=community_id).delete()
     return JsonResponse({'success': True})
 
 
