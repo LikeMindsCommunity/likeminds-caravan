833d7bba7a574a54c27ef114bb87a93940311747
apple <apple@apples-MacBook-Pro.local>
2019-08-02 15:23:16 +0530

    changing json of firebase

--- project/collabmates_api/firebase.py
+++ project/collabmates_api/firebase.py
@@ -1,8 +1,18 @@
 import pyrebase
 from django.conf import settings
 
-firebaseConfig=settings.FIREBASE_CONFIG
+FIREBASE_CONFIG = {
+    'apiKey': "AIzaSyCmu_u-n31x2WMQlWAciP5RDXGn2qMuXrg",
+    'authDomain': "collabmates-3d601.firebaseapp.com",
+    'databaseURL': "https://collabmates-3d601.firebaseio.com",
+    'projectId': "collabmates-3d601",
+    'storageBucket': "collabmates-3d601.appspot.com",
+    'messagingSenderId': "645716458793",
+    'appId': "1:645716458793:web:779debf3286d6049"
+  };
 
+firebaseConfig=FIREBASE_CONFIG
+print(firebaseConfig)
 firebase = pyrebase.initialize_app(firebaseConfig)
 
 database=firebase.database()
@@ -17,8 +27,9 @@ def update_last_answer_id(card_id,answer_id):
         'answer_id':str(answer_id)
     }
 
-    database.child("collabcards").child(card_id).update(data)
+    database.child("collabcards").child(card_id).child("collabcard").update(data)
 
     print('Data Updated successfully in firebase')
 
 
+update_last_answer_id(197,12)
