61049399c81756ae17bfa8b3bdcd30315961f217
MaheshRoyal <mahesh61437mahe@gmail.com>
2019-08-13 17:22:10 +0530

    cron job with celery

--- project/project/celery.py
+++ project/project/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.development')
@@ -20,3 +21,13 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+app.conf.beat_schedule = {
+    'pending_members_mail_at_8_AM': {
+        'task': 'collabmates_api.tasks.pending_members_mail',
+        # 'schedule': crontab(minute="*/10"),  # change to `crontab(minute=0, hour=0)` if you want it to run daily at midnight
+        'schedule':60.0,
+    },
+}
+
+app.conf.timezone = 'UTC'
