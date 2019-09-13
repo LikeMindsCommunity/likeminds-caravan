139d14b17c27c64951b1a4b18caf3ad557854096
MaheshRoyal <mahesh61437mahe@gmail.com>
2019-09-13 16:07:10 +0530

    rename tags in dashboard

--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -868,7 +868,7 @@ def add_hidden_tags(request):
                         interest_tags=interest_tags,
                         grography_tags=grography_tags)
 
-    # compute_rank.delay(community_id=community_id)
+    compute_rank.delay(community_id=community_id)
 
     return JsonResponse({'success':True})
 
@@ -1446,7 +1446,7 @@ def add_user_tags(request):
                         interest_tags=interest_tags,
                         greography_tags=grography_tags)
 
-    # compute_rank.delay(user_id = user_id)
+    compute_rank.delay(user_id = user_id)
     return JsonResponse({'success':True})
 
 
@@ -1711,7 +1711,7 @@ def map_tags(request):
     uncategorized_tag.tag_id = mapped_tag.id
     uncategorized_tag.save()
 
-    # compute_rank.delay()
+    compute_rank.delay()
     return JsonResponse({'success': True})
 
 
