512ffa08d8a85808d405e5d3273362ea5894bac0
MaheshRoyal <mahesh61437mahe@gmail.com>
2019-10-14 18:29:07 +0530

    mapping all tags

--- project/dashboard/urls.py
+++ project/dashboard/urls.py
@@ -51,5 +51,6 @@ urlpatterns = [
     path('user_metrics', user_metrics, name='user_metrics'),
     path('getfile/<int:member_id>', getfile, name='getfile'),
     path('onboarding_metrics', onboarding_metrics, name='onboarding_metrics'),
+    path('map_all_tags',map_all_tags,name='map_all_tags'),
 
 ]
