eb19860704e30a663b99477ecb6aea029bad3520
MaheshRoyal <mahesh61437mahe@gmail.com>
2019-10-18 11:01:14 +0530

    removing new url

--- project/dashboard/urls.py
+++ project/dashboard/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', dashboard, name="admin_dashboard"),
-    path('new', dashboard, name="admin_dashboard1"),
     path('update_form/<int:community_id>', update_form, name='update_form'),
     path('community_delete/<int:community_id>',community_delete,name='community_delete'),
     path('add_dashboard_admin/<int:community_id>',add_dashboard_admin,name='add_dashboard_admin'),
