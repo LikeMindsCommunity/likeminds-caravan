46c9ba992fdeea2961aa87015dc2a72578298fd0
apple <apple@apples-MacBook-Pro.local>
2019-10-22 15:56:12 +0530

    adding the firebase project for beta

--- project/collabmates_api/firebase.py
+++ project/collabmates_api/firebase.py
@@ -1,22 +1,39 @@
 import pyrebase
 from django.conf import settings
 
-FIREBASE_CONFIG = {
-    'apiKey': "AIzaSyCmu_u-n31x2WMQlWAciP5RDXGn2qMuXrg",
-    'authDomain': "collabmates-3d601.firebaseapp.com",
-    'databaseURL': "https://collabmates-3d601.firebaseio.com",
-    'projectId': "collabmates-3d601",
-    'storageBucket': "collabmates-3d601.appspot.com",
-    'messagingSenderId': "645716458793",
-    'appId': "1:645716458793:web:779debf3286d6049"
-  };
+url=settings.URL
+
+if url == 'https://beta.collabmates.com':
+    # beta firebase config
+    FIREBASE_CONFIG = {
+        'apiKey': "AIzaSyBWjDQEiYKdQbQNvoiVvvOn_cbufQzvWuo",
+        'authDomain': "collabmates-beta.firebaseapp.com",
+        'databaseURL': "https://collabmates-beta.firebaseio.com",
+        'projectId': "collabmates-beta",
+        'storageBucket': "collabmates-beta.appspot.com",
+        'messagingSenderId': "983690302378",
+        'appId': "1:983690302378:web:b2fa2c58f2351d5c1b91d3",
+        'measurementId': "G-R2PXYC9F4S"
+    }
+
+else:
+
+    # production firebase config
+    FIREBASE_CONFIG = {
+        'apiKey': "AIzaSyCmu_u-n31x2WMQlWAciP5RDXGn2qMuXrg",
+        'authDomain': "collabmates-3d601.firebaseapp.com",
+        'databaseURL': "https://collabmates-3d601.firebaseio.com",
+        'projectId': "collabmates-3d601",
+        'storageBucket': "collabmates-3d601.appspot.com",
+        'messagingSenderId': "645716458793",
+        'appId': "1:645716458793:web:779debf3286d6049"
+      }
 
 firebaseConfig=FIREBASE_CONFIG
 firebase = pyrebase.initialize_app(firebaseConfig)
-
 database=firebase.database()
 
-url=settings.URL
+
 
 def update_last_answer_id(card_id,answer_id):
 
