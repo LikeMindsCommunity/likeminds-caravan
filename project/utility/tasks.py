013c507662498f9c01afa04dd8b824c9211193fe
MaheshRoyal <mahesh61437mahe@gmail.com>
2020-01-03 12:54:49 +0530

    adding backend validation to join commnity questions and answers

--- project/utility/tasks.py
+++ project/utility/tasks.py
@@ -94,9 +94,9 @@ def new_user(member_id):
 
 
 @shared_task
-def new_member_request(member_id,commuinity_id,ref_id=None):
+def new_member_request(member_id,commuinity_id,form_response,ref_id=None,):
 
-    time.sleep(5)
+    # time.sleep(5)
     member = User.objects.get(pk=member_id)
     if ref_id:
         ref_person = User.objects.get(pk=ref_id)
@@ -139,19 +139,19 @@ def new_member_request(member_id,commuinity_id,ref_id=None):
             text = str(member_name) + ' has request to join ' + str(
                 commuinity_name) + ' community and is referred by ' + str(ref_name)
 
-    form_response = Form_response.objects.filter(user=member_id, community=commuinity_id)
+    # form_response = Form_response.objects.filter(user=member_id, community=commuinity_id)
     print("form_response =====    ", form_response)
 
-    res = {}
+    # res = {}
     for response in form_response:
-        print("question =====    ",response.data)
-        print("answer =====    ", response.response)
-        res[response.data] = response.response
+        print("question =====    ",response['key'])
+        print("answer =====    ", response['value'])
+        # res[response.data] = response.response
 
     template = get_template("mails/new_member_request.html").render({"member_name": member_name,'ref_name':ref_name,
                                                                   'subject': subject, 'commuinity_name': commuinity_name,
                                                                   'text':text,'community_link':community_link,
-                                                                     'result':res})
+                                                                     'result':form_response})
     
     if url == "https://beta.collabmates.com":
         to_list = ['mahesh61437mahe@gmail.com']
