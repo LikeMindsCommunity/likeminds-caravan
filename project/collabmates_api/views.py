4825333b3131657afc1a763d7c1fe972a785d3e2
MaheshRoyal <mahesh61437mahe@gmail.com>
2020-01-10 16:40:24 +0530

    apple login with only ID

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -2468,6 +2468,9 @@ def login(request):
                 created = True
                 mail_triger(str(user.id),request) # both mail and notification will be sent here
 
+            if not created :
+                userinfo=user[0].userinfo
+
         elif login_type == 'linkedIn':
             # if user is logging in with linkedIn
             user_name=res['firstName']['localized']['en_US'] + " " + res['lastName']['localized']['en_US']
@@ -2491,18 +2494,21 @@ def login(request):
                 created = True
                 mail_triger(str(user.id),request) # both mail and notification will be sent here
 
+            if not created :
+                userinfo=userinfo[0]
+
 
         else:
             # if user is logging in with Apple
             # converting email to lower case and removing unwanted space
-            email = res['email'].lower().strip() if 'email' in res else False
-
-            if email:
-                user = User.objects.filter(email=email)
-                user_exists = True if user.exists() else False
-            else:
-                userinfo = Userinfo.objects.filter(apple_id=res['id'])
-                user_exists = True if userinfo.exists() else User.objects.filter(pk=userinfo[0].user_id.id).exists()
+            # email = res['email'].lower().strip() if 'email' in res else False
+            # 
+            # if email:
+            #     user = User.objects.filter(email=email)
+            #     user_exists = True if user.exists() else False
+            # else:
+            userinfo = Userinfo.objects.filter(apple_id=res['id'])
+            user_exists = True if userinfo.exists() else User.objects.filter(pk=userinfo[0].user_id.id).exists()
             if not user_exists:
                 # creating a user if no user is associated with that email
                 user = create_user(user_name=res['name'], email=res['email'],id=res['id'])
@@ -2523,8 +2529,8 @@ def login(request):
                 created = True
                 mail_triger(str(user.id), request)  # both mail and notification will be sent here
 
-        if not created and email:
-            userinfo=Userinfo.objects.filter(email=email)[0]
+            if not created :
+                userinfo=userinfo[0]
 
         # get serialized user object
 
@@ -2565,7 +2571,7 @@ def create_user(user_name, email,id):
 
 def create_userinfo(user, email, user_name, profile_picture, login_type, json_to_save, city = None,apple_id=None):
     ''' function to create User-Info of a user '''
-    
+
     userinfo = Userinfo.objects.filter(email=email)
     if not userinfo.exists():
         userinfo = Userinfo()
