701912daa75a9e568d20861bc37423955693083b
apple <apple@apples-MacBook-Pro.local>
2020-01-21 13:56:39 +0530

    adding the city tags

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -42,7 +42,7 @@ from utility.utils import (decode_meta_from_url, update_tag_image,
                            #custom_cache,cache_timeout,
                            get_city_address,
                            update_user_geography_tags, create_or_categorize_tag,
-                           insert_user_home_town_tags,user_onbaord,is_IG_community)
+                           insert_user_home_town_tags,user_onbaord,is_IG_community,create_user_hometown_tag_and_related_tags)
 
 from utility.states import collabcard_seen_state,collabcard_follow_state
 
@@ -3310,7 +3310,7 @@ def save_tags_for_user_from_onboarding(category_id,tag_id,member_id):
 
     if category_id == 1:
         if tag_id.attribute_id.id == 3:
-            tag_id = insert_user_home_town_tags(user_id=member_id.id,tag=str(tag_id.tag_id))
+            tag_id =create_user_hometown_tag_and_related_tags.delay(user_id=member_id.id, tag_id=tag_id.id, new_tag=tag_id.name)
         user_tag = User_Legacy.objects.filter(tags_id=tag_id, user_id=member_id)
         if not user_tag.exists():
             user_legacy_object = User_Legacy()
