215d2a683f5a9a39b6a1f0827815a1eada7ef7f6
MaheshRoyal <mahesh61437mahe@gmail.com>
2020-01-31 11:59:52 +0530

    refactoring code for clean and simple look

--- project/utility/states.py
+++ project/utility/states.py
@@ -1,11 +1,35 @@
 
+
+# collabcard types
+class CollabcardTypes:
+    CARD_NORMAL = 0
+    CARD_INTRO = 1
+    CARD_EVENT = 2
+    CARD_POLL = 3
+
+card_types = CollabcardTypes()
 # collabcard states
+class CollabcardStates:
+    COLLABCARD_STATE_SEEN = 1
+    COLLABCARD_STATE_FOLLOW = 2
 
-collabcard_seen_state=1
-collabcard_follow_state=2
+    COLLABCARD_STATE_UNATTEND_FOLLOWING = COLLABCARD_STATE_FOLLOW
+    COLLABCARD_STATE_ATTEND_FOLLOWING = 3
+    COLLABCARD_STATE_ATTEND_UNFOLLOWING = 4
 
-# states for event collabcards
+collabcard_states = CollabcardStates()
 
-collabcard_unattend_following=2
-collabcard_attend_unfollow=4
-collabcard_attend_following=3
+
+# member state
+class MemberStates:
+    ADMIN = 1
+    TEMP_ADMIN = 2
+    PENDING_MEMBER = 3
+    MEMBER = 4
+    DECLINED_MEMBER = 5
+    UNKNOWN_NOMINATED_PROMOTER = 6
+    KNOWN_NOMINATED_PROMOTER = 7
+    INTERESTED_MEMBER = 8
+    ELIGIBLE_MEMBER = 9
+
+member_states = MemberStates()
