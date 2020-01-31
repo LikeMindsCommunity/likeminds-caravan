d188d6fdfadfb807fd7e31a3a50f50377226c8a4
MaheshRoyal <mahesh61437mahe@gmail.com>
2020-01-31 12:37:36 +0530

    refactoring report ser or collabcard mail

--- project/utility/tasks.py
+++ project/utility/tasks.py
@@ -209,17 +209,19 @@ def member_request_approval_or_denied(user_id, community_id, approved):
 
 
 @shared_task
-def send_mail_for_report_abuse__on_collabcard(user_name, collabcard_message, report_tag, community_name, community_url,
+def send_mail_for_report_abuse(user_name, collabcard_message, report_tag, community_name, community_url,
                                               reported_user_name=None, report_reason=None):
     '''function to send a mail when the user report abuse on collabcard'''
 
     subject = str(user_name) + " reported " + report_tag
 
     help_text = " for collabcard:"" for collabcard: " if not reported_user_name else " for user: "
+    reported_on = str(collabcard_message) if not reported_user_name else str(
+            reported_user_name)
     if report_reason:
-        text = str(user_name) + " reported " + str(report_tag) + help_text + str(
-            collabcard_message) + " in community " + str(community_name) + ". The feedback given By user is " + str(
-            report_reason)
+        text = str(user_name) + " reported " + str(report_tag) +\
+               help_text + reported_on + " in community " + str(community_name) +\
+               ". The feedback given By user is " + str(report_reason)
     else:
         text = str(user_name) + " reported " + str(report_tag) + help_text + str(
             collabcard_message) + " in community " + str(community_name)
@@ -235,33 +237,4 @@ def send_mail_for_report_abuse__on_collabcard(user_name, collabcard_message, rep
         to = "mahesh61437mahe@gmail.com"
     to = [to]
     send_email(subject, template, to)
-    print("Executed")
-
-
-@shared_task
-def send_mail_for_report_abuse__of_user(user_name, collabcard_message, report_tag, community_name, community_url,
-                                        reported_user_name, report_reason=None):
-    '''function to send a mail for user report abuse'''
-
-    subject = str(user_name) + " reported on CollabMates"
-    if report_reason:
-        text = str(user_name) + " reported " + str(report_tag) + " for user: " + str(
-            reported_user_name) + " in community " + str(community_name) + ". The feedback given By user is " + str(
-            report_reason)
-    else:
-        text = str(user_name) + " reported " + str(report_tag) + " for user: " + str(
-            reported_user_name) + " in community " + str(community_name)
-    context = {
-        'subject': subject,
-        'text': text,
-        'community_link': community_url
-    }
-    template = get_template("mails/report_user_abuse.html").render(context)
-    if not is_beta:
-        to = "nipun@collabmates.com"
-    else:
-        to = "mahesh61437mahe@gmail.com"
-    to = [to]
-    send_email(subject, template, to)
-
     print("Executed")
