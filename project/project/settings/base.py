dd4838be634508326008e577c32be1f81c36f51e
apple <apple@apples-MacBook-Pro.local>
2020-02-21 12:37:49 +0530

    adding admins

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -218,7 +218,7 @@ SOCIAL_AUTH_PIPELINE = (
 
 )
 
-ADMINS = [('mahesh', 'mahesh61437mahe@gmail.com')]
+ADMINS = [('mahesh', 'mahesh61437mahe@gmail.com'),('access_mahesh','accessmaheshforu@gmail.com')]
 
 
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
