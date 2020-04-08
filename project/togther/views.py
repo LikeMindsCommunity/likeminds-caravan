12af8c332df9c05ca132967f6b76684cf4d1966b
Gaurav Singh <4d.gaurav@gmail.com>
2020-04-08 22:50:36 +0530

    fQ22bHNa : using new api for filter all members filter api integration in member directory

--- project/togther/views.py
+++ project/togther/views.py
@@ -36,9 +36,6 @@ from utility.states import collabcard_states, member_states, question_states
 import re
 import ast
 
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-from rest_framework.decorators import api_view, renderer_classes
-
 url = settings.URL
 
 if not url and settings.IS_BETA:
@@ -715,8 +712,7 @@ def get_introduction_answer(community_instance, member_instance):
             return time_string
     return ""
 
-@api_view(['GET', 'POST'])
-@renderer_classes([JSONRenderer, TemplateHTMLRenderer])
+
 def members_directory(request, community_id):
 
     '''function to see members directory'''
@@ -810,19 +806,8 @@ def members_directory(request, community_id):
         if member_string == "$":
             member_string = ""
         
-        if request.accepted_renderer.format == 'html':
-            # print('request was html')
-            member_split = member_string.split("$")
-            community_instance = Community.objects.get(pk=community_id)
-            if len(option_data) > 0:
-                filtered_members = get_filtered_members(community_instance,member_split)
-            else:
-                filtered_members = get_member_details(community_instance)
-            context = {'filtered_members': filtered_members, 'filter': questions, 'option_data': dropdowns }
-            return render(request, 'filtered_members.html', context)
-        else:
-            context = {'members': member_string,'filter':questions,'option_data':dropdowns}
-            return JsonResponse(context)
+        context = {'members': member_string,'filter':questions,'option_data':dropdowns}
+        return JsonResponse(context)
 
     community_instance = Community.objects.get(pk=community_id)
     filter_list = communityQuestions.objects.filter(community=community_instance).filter(
