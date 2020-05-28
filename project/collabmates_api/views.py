2d76e040cb4f1e7f2171ea5e8156c5efdd29e8b5
PriyanshuBoss <rastogi.fresh88@gmail.com>
2020-05-28 18:22:58 +0530

    adding the collabcard

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -6084,7 +6084,7 @@ def get_chatrooms(chatroom_list,member_id):
             chatroom_instance['last_conversation'] = conversation[0]
         chatroom_instance['unseen_conversation_count'] = conversation[1]
 
-        #chatroom_instance['state'] = get_status_of_collabcard(member_id,card_instance.community.id,card_instance.id)
+        chatroom_instance['state'] = get_status_of_collabcard(member_id,card_instance.community.id,card_instance.id)
         chatrooms.append(chatroom_instance)
 
     return chatrooms
