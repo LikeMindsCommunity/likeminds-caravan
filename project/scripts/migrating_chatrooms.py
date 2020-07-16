696f3f0371e312dd96c69098d9122edc5c7aa02a
PriyanshuBoss <rastogi.fresh88@gmail.com>
2020-07-16 11:55:43 +0530

    adding the chatrooms

---
+++ project/scripts/migrating_chatrooms.py
@@ -0,0 +1,29 @@
+from togther.models import collabcardState,conversationEngage
+
+
+def get_followed_members():
+
+    followed_filter = collabcardState.objects.filter(follow_status=True).order_by('id')
+
+
+    for instance in followed_filter:
+
+        engage_filter = conversationEngage.objects.filter(card=instance.card,user=instance.user)
+
+        if not engage_filter.exists():
+
+            engage_instance = conversationEngage()
+
+            engage_instance.card = instance.card
+            engage_instance.user = instance.user
+
+            engage_instance.save()
+
+
+            print("card id",str(instance.card.id))
+
+
+
+
+
+get_followed_members()
