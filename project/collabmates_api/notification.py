d1fde3655dfdb96ddab4eac9564270ce6f107a18
himanshusaleria <himanshusaleria@gmail.com>
2020-07-19 13:47:36 +0530

    added regex to get text out of the tagged answers.

--- project/collabmates_api/notification.py
+++ project/collabmates_api/notification.py
@@ -221,9 +221,9 @@ def send_notification(fcm_token,message,is_android):
 
 
 def get_tagged_members_list(answer):
-
+    print(answer)
     tagged_users_list = re.findall("route://member/"'([0-9]+)', answer)
-    answer_text = re.split('>>', answer)[-1]
+    answer_text = re.sub(r'\|route://member/[0-9]+>>|<<', '', answer)
 
     tagged_user_names = "@" + ' @'.join(re.findall('(?<=\<\<).+?(?=\|)', answer))
     
