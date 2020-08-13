15b6ea683e4805e924cb46b95086e008823c4fa1
PriyanshuBoss <rastogi.fresh88@gmail.com>
2020-08-13 11:01:00 +0530

    changing serializers

--- project/collabmates_api/static_files.py
+++ project/collabmates_api/static_files.py
@@ -30,3 +30,48 @@ GOOGLE_PLAYSTORE = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d60
 APPLE_APPSTORE = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Fmain_website%2Fapple_store.png?alt=media&token=b1b62dfb-cde0-42c1-b801-5ed7fd0ca90b"
 
 APP_LOGO = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Fmain_website%2Flogo.png?alt=media&token=2e66c0e5-35ef-4f94-ad2b-356c78eeaaba"
+
+
+ICONS = {}
+
+ICONS['Phone No.'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fphone_no.png?alt=media&token=dada80b8-6a2c-4c47-b085-b8acf68a1c85"
+
+ICONS['Email'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Femail.png?alt=media&token=580ac8d9-bd05-421b-895f-06dca3bfa268"
+
+ICONS['LinkedIn'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2FlinkedIn.png?alt=media&token=c2482e0d-9be6-474a-923c-d3cae74ed618"
+
+ICONS['Facebook'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Ffb_alt.png?alt=media&token=af20c8c8-d51c-456b-bdab-406058e23990"
+
+ICONS['Instagram'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Finstagram.png?alt=media&token=6781087a-63f7-415f-b0ed-dacac43e423e"
+
+ICONS['Github'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fgithub.png?alt=media&token=2cadb428-e289-4cc3-948d-e5228fb47fb8"
+
+ICONS['Companies'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fcompany.png?alt=media&token=d9a64933-5d86-4544-94dc-16d410b77e2d"
+
+ICONS['Industries'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Findustry.png?alt=media&token=e6c539cb-0bbe-4f31-bf3a-9462f559b2f5"
+
+ICONS['Job designation'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fjob_designation.png?alt=media&token=d9c115e4-6ee5-4a63-b592-3b751a6a15ff"
+
+ICONS['Schools'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fschool.png?alt=media&token=3939f19a-de4a-47d6-a7ba-7de04ab0b689"
+
+#icons['Hobby'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fhobby.png?alt=media&token=81dded63-12e4-4816-89e1-00af24d9ab66"
+
+ICONS['Resident since'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fresident_since.png?alt=media&token=a9bf232a-e8a8-432c-8c88-6e283c6742d7"
+
+ICONS['Colleges'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fcollege.png?alt=media&token=aca47cf1-7395-4ee2-a534-c7b3d4115980"
+
+ICONS['Past companies'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fexperience.png?alt=media&token=22d47ed1-9bb0-40c0-a1f4-7238b112ab8e"
+
+ICONS['Portfolio'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fportfolio.png?alt=media&token=dc375d4c-72e2-45a2-9c33-8b990ef12959"
+
+ICONS['Highest education degree'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fhighest_education_degree.png?alt=media&token=23523f84-f739-4394-80d8-6f50a866908b"
+
+ICONS['City'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fcity.png?alt=media&token=b11a7a18-bff3-4f4c-96ef-66873adfe02e"
+
+ICONS['Parenting skills'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fparenting_skills.png?alt=media&token=51be5f00-a1ef-49bd-b92a-826ac2002780"
+
+ICONS['Programming languages'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fprogramming_languages.png?alt=media&token=e8885bd7-bd52-4271-a9c4-ff099734dcad"
+
+ICONS["""Child's DOB"""] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fchild_s_dob.png?alt=media&token=03c2f747-ca62-44f4-92ba-fb233e870f50"
+
+ICONS['Generic'] = "https://firebasestorage.googleapis.com/v0/b/collabmates-3d601.appspot.com/o/files%2Ficons%2Fgeneric.png?alt=media&token=823364a7-d2c8-4a74-a407-a09cd5958851"
