ba13ca3735c6f93fdffb31ba3aaa98f73831e6d6
PriyanshuBoss <rastogi.fresh88@gmail.com>
2020-08-18 13:18:23 +0530

    adding the field icon

--- project/collabmates_api/serializers.py
+++ project/collabmates_api/serializers.py
@@ -770,6 +770,8 @@ def FormResponseSerilaizer(community_id, user_id,current_user_id=None,bl=False):
 
             if response.question_title in ICONS:
                 temp['image_url'] = ICONS[response.question_title]
+            elif questions['field']:
+                temp['Generic'] = ICONS[response.question_title]
 
             new_response.append(temp)
 
