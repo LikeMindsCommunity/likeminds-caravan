b792edac4d0a33bfc4089f72516fce8080025019
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-08-25 19:50:51 +0530

    adding auto-follow in tagging

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -2419,6 +2419,16 @@ def create_chatroom_instance(res,community_instance,user_instance):
         collabcardpolls_instance.save()
 
 
+    #following the tagged member chatroom
+    tagged_members = get_tagged_members_list(res['title'])
+    tagged_member_list = tagged_members[0]
+    for user_id in tagged_member_list:
+        req_dict = {
+            'member_id': user_id,
+            'collabcard_id': card.id,
+            'status': True
+        }
+        collabcard_follow_internal(req_dict)
     return card
 
 
