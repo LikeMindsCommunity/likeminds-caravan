2f75026845a2bce469f2656b28d22904de728a96
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-08-25 17:16:55 +0530

    adding the toast flow

--- project/togther/models.py
+++ project/togther/models.py
@@ -80,9 +80,16 @@ class Members(models.Model):
     #     super(Members, self).save(*args, **kwargs)
 
 
+class communityToast(models.Model):
+
+     '''table to save the toast messages of community'''
+
+     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+     community = models.ForeignKey(Community, on_delete=models.CASCADE,null=True)
+     created_at = models.BigIntegerField(default=0)
+     toast_message = models.TextField(null=True)
 
 
-#
 
 class Userinfo(models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -1015,8 +1022,6 @@ class communityField(models.Model):
 
 
 
-
-#
 class memberNotificationFlag(models.Model):
     '''
     Model to store the flag state to send emails/push notifications of a particular user
