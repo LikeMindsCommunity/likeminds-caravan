f93177ee9c320a618e1074bc4dc56226d9d793bb
Mahesh61437 <mahesh61437mahe@gmail.com>
2020-09-10 11:08:46 +0530

    shiting backup functions to a new file

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -68,6 +68,7 @@ from .tasks import (send_email_to_nominated_admin, send_email_for_new_collabcard
                     send_community_confirmation_email)
 
 from .mails import *
+from .chatroom_backup import create_chatroom_delete_backup
 
 # CACHE_TTL = getattr(settings, 'CACHE_TTL', cache_timeout)
 
@@ -2719,7 +2720,7 @@ def chatroom_delete(request):
     '''api to delete the chatroom '''
 
     if request.method == 'GET':
-        return JsonResponse({'success': True, 'error_message':'Change HTTP method to POST'})
+        return JsonResponse({'success': False, 'error_message': 'Change HTTP method to POST'})
 
     member_id = get_member_id_from_headers(request)
     chatroom_id = request.POST.get('chatroom_id', None)
@@ -2740,7 +2741,8 @@ def chatroom_delete(request):
         collabcard_instance = Collabcard.objects.get(id=chatroom_id)
         community_id = collabcard_instance.community.id
         is_promoter = False
-        member_instance = Members.objects.filter(member_id=member_id, community_id=collabcard_instance.community).filter(
+        member_instance = Members.objects.filter(member_id=member_id,
+                                                 community_id=collabcard_instance.community).filter(
             Q(state=1))
         if member_instance.exists():
             is_promoter = True
@@ -2753,8 +2755,8 @@ def chatroom_delete(request):
             return JsonResponse(context)
 
         current_user_instance = User.objects.get(pk=member_id)
-        create_chatroom_delete_backup(collabcard_instance, current_user_instance, tag_id, reason, card_creator=is_card_creator,
-                                        promoter=is_promoter)
+        create_chatroom_delete_backup(collabcard_instance, current_user_instance, tag_id, reason,
+                                      card_creator=is_card_creator, promoter=is_promoter)
 
         delete_status = Collabcard.objects.filter(id=chatroom_id).delete()
         info_logger.info(delete_status)
@@ -2768,94 +2770,14 @@ def chatroom_delete(request):
     return JsonResponse({'success': True})
 
 
-def create_chatroom_delete_backup(card_instance, current_user_instance, tag_id, reason, card_creator=False,
-                                        promoter=False):
-    deleted_filter = deletedChatrooms.objects.filter(card_id=card_instance.id)
-
-    if deleted_filter.exists():
-        return
-    card = deletedChatrooms()
-    card.title = card_instance.title
-    card.community = card_instance.community
-    card.user = card_instance.user
-    card.type = card_instance.type
-    card.image_count = card_instance.image_count
-    card.pdf_count = card_instance.pdf_count
-    card.date_time = card_instance.date_time
-    card.duration = card_instance.duration
-
-    # for event card
-    card.location = card_instance.location
-    card.location_lat = card_instance.location_lat
-    card.location_long = card_instance.location_long
-    card.start_date = card_instance.start_date
-    card.end_date = card_instance.end_date
-    card.about = card_instance.about
-    card.co_hosts = card_instance.co_hosts
-    card.online_link = card_instance.online_link
-
-    # for poll card
-    card.multiple_select = card_instance.multiple_select
-    card.multiple_select_no = card_instance.multiple_select_no
-    card.multiple_select_state = card_instance.multiple_select_state
-    card.poll_type = card_instance.poll_type
-    card.is_poll_anonymous = card_instance.is_poll_anonymous
-    card.allow_add_option = card_instance.allow_add_option
-
-    # for chatroom header
-    card.header = card_instance.header
-
-    card.share_link = card_instance.share_link
-    card.og_tags = card_instance.og_tags
-
-    card.deleted_by_user = current_user_instance
-    text = "creator"
-    if promoter:
-        text = "community manager"
-    card.deleted_by_text = text
-    card.deleted_by_creator = card_creator
-    card.deleted_by_promoter = promoter
-    if reason:
-        card.reason = reason
-    if tag_id:
-        tag = Report_Tags.objects.filter(tag_id=tag_id)
-        if tag.exists():
-            card.tag = tag[0]
-
-    card.date_epoch = time.time()  # card creation time
-    card.card_id = card_instance.id
-    card.save()
-
-    create_chatroom_participants_backup(card_instance=card_instance, deleted_card_instance=card)
-
-
-def create_chatroom_participants_backup(card_instance, deleted_card_instance):
-    participants_list = collabcardState.objects.filter(card=card_instance).filter(follow_status=True).distinct("user")
-    for participant in participants_list:
-        create_collbacard_state_backup(participant, deleted_card_instance=deleted_card_instance)
-
-def create_collbacard_state_backup(collabcard_state_instance, deleted_card_instance):
-
-    backup_instance = CollabcardStateBackup()
-    backup_instance.card = deleted_card_instance
-    backup_instance.community = collabcard_state_instance.community
-    backup_instance.user = collabcard_state_instance.user
-    backup_instance.state = collabcard_state_instance.state
-    backup_instance.remove = collabcard_state_instance.remove
-    backup_instance.seen_status = False
-    backup_instance.mute_status = collabcard_state_instance.mute_status
-    backup_instance.follow_status = collabcard_state_instance.follow_status
-    backup_instance.is_guest = collabcard_state_instance.is_guest
-    backup_instance.source = collabcard_state_instance.source
-    backup_instance.save()
-
 def fetch_deleted_chatroom(request):
     """ function to update polls of a card for user """
     if request.method == 'GET':
         member_id = get_member_id_from_headers(request)
         user_instance = User.objects.get(pk=member_id)
 
-        deleted_chatrooms = CollabcardStateBackup.objects.select_related('card', 'card__tag').filter(user=user_instance).filter(seen_status=False)
+        deleted_chatrooms = CollabcardStateBackup.objects.select_related('card', 'card__tag').filter(
+            user=user_instance).filter(seen_status=False)
 
         toast_title = ''
         title = ''
@@ -2909,12 +2831,13 @@ def fetch_deleted_chatroom(request):
             "deleted_chatrooms_count": deleted_chatrooms_count,
         }
 
-        #updating deleted chatroom seen status to True to not show them again
+        # updating deleted chatroom seen status to True to not show them again
         updated_chatrooms = CollabcardStateBackup.objects.filter(user=user_instance).update(seen_status=True)
 
         return JsonResponse({"deleted_chatrooms": final_dict})
 
     return JsonResponse({"success": False})
+
 
 # api to deprecate
 @csrf_exempt
