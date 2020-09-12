bc0d1c1815a057e108b26dcdd70401f9d9f515e6
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-09-12 12:23:18 +0530

    adding the screen

--- project/collabmates_api/static_text.py
+++ project/collabmates_api/static_text.py
@@ -205,7 +205,7 @@ report = {'id': 10, 'title': 'Report'}
 
 
 #get onboarding examples
-ONBOARDING_EXAMPLES = [
+INTRODUCTION_EXAMPLES = [
 
 
     {
@@ -233,6 +233,38 @@ ONBOARDING_EXAMPLES = [
     }
 
 ]
+
+
+ONBOARDING_EXAMPLES = [
+
+
+    {
+    "header":"Sample community purposes",
+    "sub_header" : "Here are few examples of other communities’ purpose.",
+    "title" : "IITD Alums in Gurgaon",
+    "sub_title":"""This community is for IITD alumni currently living in Gurgaon and nearby areas. 
+Anytime if you are looking to exchange referrals or maybe want to have a small get together just create a chatroom in the community with relevant content and interested community members will participate in it."""
+
+    },
+
+    {
+    "header":"Sample community purposes",
+    "sub_header" : "Here are few examples of other communities’ purpose.",
+    "title" : "Python Developers in Mumbai",
+    "sub_title":"Welcome tech lovers far and wide! We’re an online and in-person python-enthusiast group hosting live speaking events on a range of tech topics. You can join us in person if possible or on one of our live streams. Look out for our virtual happy hours and other networking events."
+
+    },
+
+    {
+    "header":"Sample community purpose",
+    "sub_header" : "Here are few examples of other communities’ purpose.",
+    "title" : "Adventure Sports Enthusiasts",
+    "sub_title":"This is a group for anyone interested in adventure sports like hiking, rock climbing, camping, kayaking, bouldering, etc. All skill levels are welcome. We started this group to meet other outdoor enthusiasts. Looking forward to exploring the outdoors with everybody."
+
+    }
+
+]
+
 
 
 
