b46f97dcf40de348102d94fffc13d70a9a14183c
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-09-19 13:49:47 +0530

    adding the followed chatrooms

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -6789,10 +6789,10 @@ def fetch_chatroom_feed_version_1(request):
         elif scroll_direction == 1:  # downward scroll
 
             if active:
-                downward = state_filter.filter(card__gt=chatroom_id).filter(
+                downward = state_filter.filter(card__gt=chatroom_id,user=member_id).filter(
                     Q(expiry_time=None) | Q(expiry_time__gt=current_time))[:5]
             else:
-                downward = state_filter.filter(card__gt=chatroom_id).filter(
+                downward = state_filter.filter(card__gt=chatroom_id,user=member_id).filter(
                     ~Q(expiry_time=None) & Q(expiry_time__lte=current_time))[:5]
 
             chatrooms = get_chatrooms_version_1(downward, member_id,active)
