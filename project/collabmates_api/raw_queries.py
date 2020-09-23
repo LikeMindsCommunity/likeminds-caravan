384a543cf109ef7e42b176e4aa98ea541ef8c901
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-09-23 17:11:41 +0530

    adding my chatroom count

--- project/collabmates_api/raw_queries.py
+++ project/collabmates_api/raw_queries.py
@@ -197,6 +197,33 @@ def get_inactive_followed_chatrooms_count(user_id,current_time):
 
 
 
+def get_active_my_chatrooms_count(user_id,current_time):
+
+    '''function to give the count of active my chatrooms'''
+
+    try:
+
+
+        conn = get_connection()
+        curr = conn.cursor()
+        sql = """select count(id) from togther_conversationEngage where user_id=%s and card_id  in
+                     (select card_id from togther_collabcardState where user_id = %s and follow_status = True and (remove_id is null)
+                    and (expiry_time is null or expiry_time > %s) 
+                   )""" % (
+        str(user_id), str(user_id), str(current_time))
+
+        curr.execute(sql)
+        count = curr.fetchone()
+        curr.close()
+        conn.close()
+
+        return count[0]
+
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL  ", error)
+
+
 
 def get_active_followed_chatrooms(user_id,current_time,page,limit=10):
 
