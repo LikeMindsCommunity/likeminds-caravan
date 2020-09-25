47c1372372ab61e2df1a7b254d0d7357e1ef89e9
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-09-25 16:20:14 +0530

    adding the comunities

--- project/collabmates_api/urls.py
+++ project/collabmates_api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 #     path('mail/', TemplateView.as_view(template_name='mails/verify_email_template.html')),
     path('mail/', TemplateView.as_view(template_name='mails/email_otp.html')),
 
-    path('communities', api_views.communities, name="communities"),
+    #path('communities', api_views.communities, name="communities"),
     path('your_communities/<int:user_id>', api_views.your_communities, name="your_communities"),
 
     path('community/<int:community_id>', api_views.community, name="community"),
