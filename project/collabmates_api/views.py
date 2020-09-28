9d91fac44a0292178a410831db45a0a897eb31cc
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-09-28 11:05:28 +0530

    adding the unseen count

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -338,7 +338,8 @@ def your_communities(request, user_id):
 
         # if community['collabcard_unseen'] > 0:
             # header_images = get_new_chatroom_member_images(member_id=member_id,community_id=each_community.community_id.id)
-        if each_community.new_chatroom_users:
+        if community['collabcard_unseen'] > 0:
+            #community['new_chatroom_users'] = json.loads(each_community.new_chatroom_users)
             community['new_chatroom_users'] = get_new_chatroom_member_images(member_id=member_id,community_id=each_community.community_id.id)
 
         my_community.append(community)
