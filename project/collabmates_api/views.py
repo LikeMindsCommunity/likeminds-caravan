a02f7ea2c58e5e871807ec8ba1df5385681ecf18
Himanshu Saleria <himanshusaleria@TheMachine.local>
2020-10-10 13:10:59 +0530

    changed member to user

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -4061,16 +4061,21 @@ def approve_or_decline_private_community(req_dict, request):
             removedMembers.objects.filter(community=req_dict['community_id'], member=req_dict['member_id']).delete()
 
 
+
             #send sms
-            member_instance = Members.objects.get(member_id = int(req_dict['member_id']))
-            new_user_instance = member_instance.member_id
-            new_user_name = get_first_name_from_name(new_user_instance.userinfo.name)
-            mobile_filter = userMobiles.objects.filter(user_id=new_user_instance.id)
-            print(mobile_filter)
-            for instance in mobile_filter:
-                print("sending sms here")
-                phone_no = str(instance.country_code) + str(instance.mobile_no)
-                send_community_confirmation_sms.delay(phone_no,community.name,new_user_name,new_user_instance.id)
+            notification_list = [
+                'mail_has_installed_app'
+            ]
+            if check_notification_flag(req_dict['member_id'], notification_list, card_id=None, community_id=None):
+                new_user_instance = User.objects.get(id = int(req_dict['member_id']))
+                new_user_name = get_first_name_from_name(new_user_instance.userinfo.name)
+                mobile_filter = userMobiles.objects.filter(user_id=new_user_instance.id)
+                # print(mobile_filter)
+                for instance in mobile_filter:
+                    # print("sending sms here")
+                    info_logger.log('sending sms for community approval to',instance.id)
+                    phone_no = str(instance.country_code) + str(instance.mobile_no)
+                    send_community_confirmation_sms.delay(phone_no,community.name,new_user_name,new_user_instance.id)
 
 
             # sending mails and notifications
