0aab9ebaddeea681f8d0e7e3db56d1eb4094f5bd
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-10-16 17:37:03 +0530

    adding the beta otp

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -8443,6 +8443,20 @@ def verify_otp(request):
         else:
             return JsonResponse({'success': False, 'error_message': "Wrong otp"})
 
+
+    if settings.IS_BETA:
+        if otp == "9999":
+            context = {}
+            context['success'] = True
+            mobile_filter = userMobiles.objects.filter(mobile_no=mobile_no)
+            context['profile_exists'] = mobile_filter.exists()
+            if mobile_filter.exists():
+                context['user'] = get_logged_in_user(user_instance=mobile_filter[0].user)
+                context['access'] = is_user_community_part(context['user']['id'])
+                return JsonResponse(context)
+            else:
+                return JsonResponse({'success': False, 'error_message': "Wrong otp"})
+
     # for existing users flow
     member_id = get_member_id_from_headers(request)
 
