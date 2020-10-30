96e006cd7ef16535c8af041173f60311e2da26b8
Mahesh61437 <mahesh61437mahe@gmail.com>
2020-10-30 17:07:33 +0530

    rejoin check bug fix

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -1191,7 +1191,7 @@ def join_promoter_created_community_version_1(res, request):
             # insert private link dropoff here
             time_in_hrs = 2
 
-            if validate_time:
+            if True:
                 is_private_link = True
                 # saving moderation history
                 print("saving history 1")
@@ -1200,7 +1200,7 @@ def join_promoter_created_community_version_1(res, request):
                     print("saving history 2 ---  ", res['shared_by'])
 
                     history_type = moderation_history_types.APPLIED_PRIVATE_LINK
-                    if check_user_rejoin(user=user_instance, community=community):
+                    if check_user_rejoin(user=user_instance, community=community_instance):
                         history_type = moderation_history_types.REJOINED_COMMUNITY_PRIVATE_LINK
                         update_followed_for_rejoined_member(user_instance, community)
 
