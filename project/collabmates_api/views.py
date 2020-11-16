5fac175e3d36ea72b33d316905886b70d22a7208
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-11-16 12:05:01 +0530

    adding the community api

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -13432,14 +13432,25 @@ class SyncCommunities(APIView):
         chatroom_id = query_params.get('chatroom_id', '')
         community_id = query_params.get('community_id', '')
 
-        member_filter = Members.objects.order_by('-id')
-        member_filter = pagination(member_filter,page,paginate_by=paginate_by)
+        if last_updated:
+            engage_filter = Member_Engage.objects.filter(member_id=member_id,updated_at__gt = last_updated).order_by('id')
+        else:
+            engage_filter = Member_Engage.objects.filter(member_id=member_id).order_by('id')
+
+        engage_filter = pagination(engage_filter,page,paginate_by=paginate_by)
         community_list = []
-        for data in member_filter:
+        max_last_updated = 0
+        for data in engage_filter:
             temp = CommunitySerializer(data.community_id,current_user_id=member_id)
+
+            if max_last_updated < data.updated_at:
+                max_last_updated = data.updated_at
+
             community_list.append(temp)
 
-
+        if max_last_updated:
+            context = {'communities': community_list,'max_last_updated':max_last_updated}
+            return JsonResponse(context)
         return JsonResponse({'communities':community_list})
 
 
