65b3e0acaf4273780de64034c082c491d949316d
Ketan Yadav <ketan@likeminds.community>
2020-11-18 17:55:23 +0530

    Add landing page module

---
+++ project/collabmates_api/landing_page/views_manager.py
@@ -0,0 +1,17 @@
+import abc
+
+from django.http import JsonResponse
+
+
+class ViewsManager(metaclass=abc.ABCMeta):
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'get_member_communities') and
+                callable(subclass.get_member_communities) or
+                NotImplemented)
+
+    @abc.abstractmethod
+    def get_member_communities(self) -> JsonResponse:
+        """Get communities of the member"""
+        raise NotImplementedError
