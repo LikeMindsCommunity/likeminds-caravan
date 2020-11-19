51a378dc21a2a725f2ce1fb83aeb77aa6a361db1
Ketan Yadav <ketan@likeminds.community>
2020-11-19 16:25:28 +0530

    Add memeber id check

--- project/collabmates_api/landing_page/views_impl.py
+++ project/collabmates_api/landing_page/views_impl.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 
+from collabmates_api.landing_page.member_community_impl import MemberCommunityImpl
 from collabmates_api.landing_page.views_manager import ViewsManager
 from collabmates_api.utilities.request_utilities import RequestUtilities
 from collabmates_api.utilities.number_utilities import NumberUtilities
-from collabmates_api.landing_page.member_community_impl import MemberCommunityImpl
+from collabmates_api.views import get_error_context
 
 
 class ViewsImpl(ViewsManager):
@@ -16,6 +17,13 @@ class ViewsImpl(ViewsManager):
         page = NumberUtilities.get_integer_from_string(page, 1)
 
         member_id = RequestUtilities.get_member_id_from_headers(request)
+        if not member_id:
+            """
+            TODO: this sends HTTP code: 200 OK response instead of 400 Bad Request
+            create exception handling module
+            """
+            context = get_error_context(False, "member id missing in request")
+            return JsonResponse(context)
 
         member_community_manager = MemberCommunityImpl(member_id, page)
         member_community_manager.extract_member_communities()
