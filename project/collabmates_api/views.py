2adcd7207398e545013beafa04f66e4095461be2
Mahesh61437 <mahesh61437mahe@gmail.com>
2020-12-07 22:24:35 +0530

    correcting the invite sentence

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -953,7 +953,7 @@ def questions(request):
     try:
         shared_by_user = User.objects.get(pk=shared_by)
         shared_by_user_name = shared_by_user.userinfo.name
-        title = f"{shared_by_user_name} invited you the join this community"
+        title = f"{shared_by_user_name} invited you to join this community"
     except:
         info_logger.info(f"shared by user id does not exist in DB. shared by ---> {shared_by} ")
 
