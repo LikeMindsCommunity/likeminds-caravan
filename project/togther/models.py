2f77b79bfb8b5fd2f0ca76b78c8078b10e16faad
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-12-10 11:14:06 +0530

    adding the your communities apis

--- project/togther/models.py
+++ project/togther/models.py
@@ -576,6 +576,13 @@ class Member_Engage(models.Model):
     click_state = models.IntegerField(default=0)
     new_chatroom_users = models.TextField(null=True)
     rights_list = models.TextField(null=True)
+    order_time = models.BigIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if self.order_time == 0:
+            self.order_time = int(time.time() * 1000)
+        self.order_time = int(time.time() * 1000)
+        super(Member_Engage, self).save(*args, **kwargs)
 
 # community lpig
 
