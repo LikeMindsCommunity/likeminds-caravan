16595a48a4868faaf37698e50065cd89a3d2d809
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2020-12-14 20:19:03 +0530

    adding production in celert

--- project/project/celery.py
+++ project/project/celery.py
@@ -9,12 +9,14 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 # set the default Django settings module for the 'celery' program.
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.beta')
+
 
 if settings.IS_BETA:
     app = Celery('project', backend='amqp', broker=os.getenv('BROKER_URL'))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.beta')
 else:
     app = Celery('project')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.production')
 # app = Celery('project', backend='amqp', broker=os.getenv('BROKER_URL'))
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
