c11ac51ac594da787779b45d4e25dbc989a93693
PriyanshuBoss <rastogi.fresh88@gmail.com>
2020-12-30 11:49:13 +0530

    sending participant coutn (#3132)
    Co-authored-by: priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>

--- project/collabmates_api/chatroom/chatroom_impl.py
+++ project/collabmates_api/chatroom/chatroom_impl.py
@@ -169,6 +169,11 @@ class ChatroomImpl(ChatroomManager):
 
         save_the_latest_conversation(card_instance, self.get_member_id())
 
+    def _fetch_count_of_chatroom_participants(self, card_instance):
+
+        participant_count = collabcardState.objects.filter(follow_status=True, card=card_instance).count()
+
+        return participant_count
 
     def fetch_chatroom(self):
 
@@ -201,6 +206,7 @@ class ChatroomImpl(ChatroomManager):
         chatroom_obj['total_response_count'] = self._fetch_total_response_count(card_instance)
         chatroom_obj['community'] = ChatroomHelper.fetch_serialized_community(card_instance, user_instance, self.get_member_id())
         chatroom_obj['unread_messages'] = self._fetch_number_of_unread_messages(card_instance, user_instance)
+        chatroom_obj['participant_count'] = self._fetch_count_of_chatroom_participants(card_instance)
         self._save_external_seen_in_chatroom_state(card_instance, user_instance)
         self._save_latest_conversation_on_screen(card_instance)
 
