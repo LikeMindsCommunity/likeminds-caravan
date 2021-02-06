bc61afceff37c176177c299cdb3ba5c4c6af3ae0
Mahesh Royal <41134562+Mahesh61437@users.noreply.github.com>
2021-02-06 15:36:37 +0530

    LM-1367 - Event start notification (#3283)

--- project/utility/time_utilities.py
+++ project/utility/time_utilities.py
@@ -1,4 +1,7 @@
 import time
+from typing import Union
+from dateutil import tz
+from datetime import datetime, timedelta
 
 
 class TimeUtilities:
@@ -19,3 +22,31 @@ class TimeUtilities:
     def current_time_in_milliseconds() -> int:
         return int((time.time() * 1000))
 
+    @staticmethod
+    def convert_milliseconds_to_sec(millisec: Union[int, str]) -> int:
+        return millisec // 1000
+
+    @staticmethod
+    def get_indian_time_zone():
+        return tz.gettz('IST')
+
+    @staticmethod
+    def convert_epoch_to_datetime_in_IST(epoch_time):
+        tz_ist = TimeUtilities.get_indian_time_zone()
+        return datetime.fromtimestamp(epoch_time).replace(tzinfo=tz_ist)
+
+    @staticmethod
+    def add_minutes_to_datetime(date_time, minutes):
+        return date_time + timedelta(minutes=minutes)
+
+    @staticmethod
+    def add_hours_to_datetime(date_time, hours):
+        return date_time + timedelta(hours=hours)
+
+    @staticmethod
+    def subtract_minutes_from_datetime(date_time, minutes):
+        return date_time - timedelta(minutes=minutes)
+
+    @staticmethod
+    def subtract_hours_from_datetime(date_time, hours):
+        return date_time - timedelta(hours=hours)
