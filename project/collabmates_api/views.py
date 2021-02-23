51203afbaf0c69901d84369d83eff0500837ee95
PriyanshuBoss <rastogi.fresh88@gmail.com>
2021-02-23 16:32:19 +0530

    Lm 1291 intro room segregation (#3378)

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -8347,7 +8347,12 @@ def fetch_chatroom_feed_version_1(request):
         last_seen = state_filter.filter(~Q(state=0)).order_by('-card_id')
 
         if not last_seen.exists():
-            chatroom_list = pagination(state_filter, page, paginate_by=5)
+
+            if active:
+                chatroom_list = state_filter.filter(Q(expiry_time=None) | Q(expiry_time__gt=current_time))[:5]
+            else:
+                chatroom_list = state_filter.filter(~Q(expiry_time=None) & Q(expiry_time__lte=current_time))[:5]
+
             chatrooms = get_chatrooms_version_1(chatroom_list, member_id, is_ios=is_ios)
         else:
 
