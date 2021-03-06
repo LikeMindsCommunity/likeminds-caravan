f628b4042bc92ed623000e616bb931adee13953d
PriyanshuBoss <rastogi.fresh88@gmail.com>
2021-03-06 14:05:32 +0530

    Lm 1881 dublicate intro chatroom issue (#3419)
    * adding the intro
    
    * adding the intro room
    
    * adding the data
    
    Co-authored-by: priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>

--- project/scripts/intro_room_segregation.py
+++ project/scripts/intro_room_segregation.py
@@ -18,10 +18,11 @@ from external_services.logging.logging_wrapper import LoggingWrapper
 info_logger = LoggingWrapper.get_instance()
 
 
-def perform_soft_delete_for_dublicate_intro_rooms():
-    community_list = [49792, 49825, 49813, 49751, 49722, 49788, 49694]
+def perform_soft_delete_for_dublicate_intro_rooms(community_list):
+
 
     for community_id in community_list:
+
         master_intro = ModelUtilities.get_model_filter(Collabcard, {'type': 9, 'community': community_id})
 
         if master_intro:
@@ -36,11 +37,9 @@ def perform_soft_delete_for_dublicate_intro_rooms():
             print(log)
 
 
-
-
 def post_introductions_card_for_communities(community_list):
-    for community in community_list:
 
+    for community in community_list:
         master_community_list = []
         member_filter = ModelUtilities.get_model_filter(Members,
                                                         {'state': 1, 'is_owner': True, 'community_id': community})
@@ -112,6 +111,7 @@ def save_individual_intro_card_in_cache(community_list):
 
 def saving_updated_at_for_intro_rooms_for_syncing():
     card_filter = ModelUtilities.get_model_filter(Collabcard, {'type': 1})
+
     for card_instance in card_filter:
         current_time = TimeUtilities.current_time_in_sec()
         ModelUtilities.model_update(collabcardState, {'card': card_instance}, {'updated_at': current_time})
@@ -123,8 +123,10 @@ def saving_updated_at_for_intro_rooms_for_syncing():
 
 def create_introduction_card_conversations():
     # community_list = [49792, 49825, 49813, 49751, 49722, 49788, 49694]
+
     community_list = [49632]  # LMCM community
     start_time = TimeUtilities.current_time_in_sec()
+    perform_soft_delete_for_dublicate_intro_rooms(community_list)
     post_introductions_card_for_communities(community_list)
     #save_individual_intro_card_in_cache(community_list)
     end_time = TimeUtilities.current_time_in_sec()
