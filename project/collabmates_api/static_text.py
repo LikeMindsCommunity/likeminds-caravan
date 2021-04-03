65ad8f2ed5557bab1d4dd744f36fb0b82e25967e
PriyanshuBoss <rastogi.fresh88@gmail.com>
2021-04-03 19:04:37 +0530

    adding the time (#3564)
    Co-authored-by: priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>

--- project/collabmates_api/static_text.py
+++ project/collabmates_api/static_text.py
@@ -5,8 +5,8 @@ if SERVER_URL is None:
     SERVER_URL = 'https://beta.likeminds.community'
 
 # variables
-HOURS_24 = 86400
-#HOURS_24 = 200
+#HOURS_24 = 86400
+HOURS_24 = 60
 
 chatroom_actions_creator_mute = [
 
