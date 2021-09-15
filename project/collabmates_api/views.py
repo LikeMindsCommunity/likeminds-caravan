739841b2bce610f81a7227e58c23933f387e0bf1
a-grg <86524580+a-grg@users.noreply.github.com>
2021-09-15 14:24:32 +0530

    LM-3643 Removed DM chatrooms from search (#4031)
    * Added create DM chatroom in join_community.
    
    * Added chatroom with user support V1.
    
    * Added new card_type to fetch_feed API.
    
    * Added Community Settings Right for Direct Messages.
    
    * Added two API's i.e. fetch_dm_home and update_dm_tutorial.
    
    * Adde DM Message Feed.
    
    * Added new API api/community_member/show_dm
    
    * Added chatroom_with_user in syn_chatrooms API.
    
    * Added chatroom_with_user object in response of chatroom/fetch API.
    
    * Added chatroom actions for DM user and also corrected the chatroom_with_user value to user_id instead of member.id
    
    * Corrected value in dm chatroom actions dictionary.
    
    * Added support for DM chatroom in case of member left or removed.
    
    * Added support for DM chatrooms when user is not CM.
    
    * Changed DM flow for chatroom actions and community_member/fetch_feed.
    
    * Added is_private key, celery function of create_member_dm_chatroom.
    
    * Added is_private in celery function.
    
    * Modified Remove User and CM with adding is_private key.
    
    * Added dm, non dm in my_chatrooms API.
    
    * Added message support in my_chatrooms API.
    
    * Added member becomes CM case.
    
    * Added member becomes CM v2.
    
    * Added deepanshu comments.
    
    * utility/celery_tasks.py
    
    * Added DM chatrooms for member state profile unavailable.
    
    * Added script for backfilling of data.
    
    * Corrected unseen count in my_chatrooms API.
    
    * Added CM left the community.
    
    * Added more error handling, resolved minor bugs.
    
    * Resolved ketan comments v1.
    
    * Added formatting of SQL code as mentioned by ketan.
    
    * Refactoring of code.
    
    * Refactoring of code.
    
    * Added ketan comments changes v3.
    
    * Reformat raw queries.
    
    * Resolved sync_chatrooms API.
    
    * Error in create_member_dm_chatroom.
    
    * Added more robust script for filling previous data.
    
    * Modified the key value for is_private and chatroom_with_user keys.
    
    * Added routes to messages.
    
    * Added is_cm key in response of fetch_dm_home.
    
    * Added community_id and chatroom_id in show_dm for member_profile
    
    * Modified my_chatrooms API.
    
    * Added version code in sync_chatrooms API.
    
    * Removed DM chatrooms from home communities chatrooms count.
    
    * Modified my_chatrooms API.
    
    * Modified my_chatrooms API.
    
    * Modified my_chatrooms API.
    
    * Removed version code from sync_chatrooms API.
    
    * Added version code from sync_chatrooms API.
    
    * Added show_dm support to home_communities.
    
    * Modified my_chatrooms API.
    
    * resolved merge comflict with development.
    
    * Formatted.
    
    * Formatted.
    
    * Formatted.
    
    * Formatted.
    
    * Modified fetch_dm_feed.
    
    * Removed intorduction attachement from DM chatrooms.
    
    * Modifications suggested by clients.
    
    * Added subtitle in DM settings.
    
    * Added subtitle in DM settings.
    
    * Re-architectured M2CM.
    
    * Modified backfill script by adding deleting of existing records.
    
    * Added is_cm in home_communities, member_serializer for chatroom_with_user.
    
    * Resolved ketan comments.
    
    * Resolved bug in home_communities.
    
    * Resolved serialize_chatroom bug.
    
    * Removed DM right in creating a community.
    
    * Removed DM chatrooms from search.
    
    * Added DM conversation states to last_conversation object.
    
    Co-authored-by: Ankit Garg <ankitgarg@Ankits-MacBook-Pro.local>

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -5309,7 +5309,13 @@ def save_the_latest_conversation(card_instance, user_id):
         return {'last_conversation': None}
 
     last_conversation = card_answers.objects.filter(card=card_instance).\
-        filter(Q(state=conversation_states.CONVERSATION_POLL) | Q(state=conversation_states.ANSWER)).last()
+        filter(Q(state=conversation_states.CONVERSATION_POLL) |
+               Q(state=conversation_states.ANSWER) |
+               Q(state=conversation_states.CONVERSATION_DIRECT_MESSAGE_MEMBER_REMOVED_OR_LEFT) |
+               Q(state=conversation_states.CONVERSATION_DIRECT_MESSAGE_CM_REMOVED) |
+               Q(state=conversation_states.CONVERSATION_DIRECT_MESSAGE_MEMBER_BECOMES_CM_DISABLE_CHAT) |
+               Q(state=conversation_states.CONVERSATION_DIRECT_MESSAGE_CM_BECOMES_MEMBER_ENABLE_CHAT) |
+               Q(state=conversation_states.CONVERSATION_DIRECT_MESSAGE_MEMBER_BECOMES_CM_ENABLE_CHAT)).last()
 
     if last_conversation:
         user_instance = User.get_user_or_raise_exception(user_id)
