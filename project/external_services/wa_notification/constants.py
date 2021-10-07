705af0bdb30d0d2b58c3b43c42c3712f59133282
a-grg <86524580+a-grg@users.noreply.github.com>
2021-10-07 11:52:30 +0530

    [LM-4533] Payment Pages (#4092)
    Added API's for email and whatsApp

--- project/external_services/wa_notification/constants.py
+++ project/external_services/wa_notification/constants.py
@@ -15,10 +15,30 @@ WATI_NOTIFICATION_CONST = {
         "template_name": "Template name that was approved by whatsapp | mandatory"
     },
     'WATI_BROADCAST_METHOD': 'POST',
-    'TEMPLATE_NAMES':{
-        'EVENT_REMINDER': 'online_event_reminder_v1'
+    'TEMPLATE_NAMES': {
+        'EVENT_REMINDER': 'online_event_reminder_v1',
+        'PAYMENT_PAGE_SUCCESS': 'payment_page_successfull_v1',
+        'PAYMENT_PAGE_FAILED': 'failed_payment_member_v5',
     },
-    'BROADCAST_NAMES':{
-        'EVENT_REMINDER': 'event_reminder_core'
-    }
+    'BROADCAST_NAMES': {
+        'EVENT_REMINDER': 'event_reminder_core',
+        'PAYMENT_PAGE_SUCCESS': 'payment_page_success',
+        'PAYMENT_PAGE_FAILED': 'payment_page_failed'
+    },
+    'WATI_BROADCAST_BULK_URL': 'https://live-server-876.wati.io/api/v1/sendTemplateMessages',
+    'WATI_BROADCAST_BULK_SCHEMA': {
+        "receivers": [
+            {
+                "whatsappNumber": "Number on which message to be send",
+                "customParams": [
+                    {
+                        "name": "parameter name",
+                        "value": "parameter value",
+                    }
+                ]
+            }
+        ],
+        "broadcast_name": "Broadcast name that you want to set | mandatory",
+        "template_name": "Template name that was approved by whatsapp | mandatory"
+    },
 }
--- project/external_services/wa_notification/wati_api_client.py
+++ project/external_services/wa_notification/wati_api_client.py
@@ -9,10 +9,12 @@ from external_services.wa_notification.wati_api_manager import WAApiManager
 from external_services.logging.logging_wrapper import LoggingWrapper
 from utility.number_utilities import NumberUtilities
 
+
 class WAApiClient(WAApiManager):
 
     logger = LoggingWrapper.get_instance()
     URL = WATI_NOTIFICATION_CONST.get('WATI_BROADCAST_URL')
+    BULK_URL = WATI_NOTIFICATION_CONST.get('WATI_BROADCAST_BULK_URL')
     WA_BROADCAST_METHOD = WATI_NOTIFICATION_CONST.get('WATI_BROADCAST_METHOD')
     AUTH_TOKEN = settings.WA_API_KEY
 
@@ -42,15 +44,53 @@ class WAApiClient(WAApiManager):
             if hasattr(api_response, 'status_code') and \
                     NumberUtilities.get_integer_from_string(api_response.status_code) != 200:
                 message = "Error response from wati broadcast api:\n%s" % api_response.content.decode('utf-8')
+
                 raise Exception(message)
 
         except Exception:
             message = "Wa broadcast api call failed:\n%s" % traceback.format_exc()
             self.logger.error(message)
 
+    @classmethod
+    def call_wa_bulk_broadcast_api(self, receivers_list, template_name, broadcast_name) -> None:
+
+        try:
+            api_payload = self._create_bulk_api_payload(self, template_name, broadcast_name, receivers_list)
+            api_endpoint = self._create_bulk_api_endpoint(self)
+
+            api_response = requests.request(self.get_wa_broadcast_method(self),
+                                            api_endpoint,
+                                            headers=api_payload.get('headers'),
+                                            data=json.dumps(api_payload.get('body')))
+
+            if hasattr(api_response, 'status_code') and \
+                    NumberUtilities.get_integer_from_string(api_response.status_code) != 200:
+                message = "Error response from wati bulk broadcast api:\n%s" % api_response.content.decode('utf-8')
+
+                raise Exception(message)
+
+        except Exception:
+            message = "Wa bulk broadcast api call failed:\n%s" % traceback.format_exc()
+            self.logger.error(message)
+
     def _create_api_payload(self, template_name: str, broadcast_name: str, parameters: list) -> dict:
         api_body = WATI_NOTIFICATION_CONST.get('WATI_BROADCAST_SCHEMA')
         api_body['parameters'] = parameters
+        api_body['broadcast_name'] = broadcast_name
+        api_body['template_name'] = template_name
+
+        api_headers = self._create_api_headers(self)
+
+        payload = {
+            'headers': api_headers,
+            'body': api_body
+        }
+
+        return payload
+
+    def _create_bulk_api_payload(self, template_name: str, broadcast_name: str, receivers_list: list) -> dict:
+        api_body = WATI_NOTIFICATION_CONST.get('WATI_BROADCAST_BULK_SCHEMA')
+        api_body['receivers'] = receivers_list
         api_body['broadcast_name'] = broadcast_name
         api_body['template_name'] = template_name
 
@@ -71,6 +111,9 @@ class WAApiClient(WAApiManager):
 
     def _create_api_endpoint(self, phone) -> str:
         return self.URL % phone
+
+    def _create_bulk_api_endpoint(self) -> str:
+        return self.BULK_URL
 
     def _get_user_phone(self, user_data) -> str:
         return user_data['phone']
