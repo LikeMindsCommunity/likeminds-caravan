910b343d5a169c3b1e56751728f5fa57b26c8456
Naman Jain <89894541+naman-likeminds@users.noreply.github.com>
2021-10-21 17:28:36 +0530

    [LM-4532] removal of inactive chatrooms (#4140)
    * Remove support of `api/fetch_chatroom_inactive`
    
    * Stop sending `show_active` in `chatroom/fetch`
    
    * Stop sending `mark_inactive` and `mark_active` Chatroom Actions
    
    * `api/set_chatroom_active` will not perform any action
    
    * removing inactive_chatroom_count from api/v1/my_chatrooms
    
    * removed updation of expiry_time in create_chatroom_state_instance
    
    * removed active_chatroom_count and inactive_chatroom_count in api/fetch_chatroom_feed and api/v1/fetch_chatroom_feed
    
    * removed active from api/fetch_chatroom_feed
    
    * removed active from api/v2/fetch_chatroom_feed
    
    * removed set_chatroom_active_or_inactive and its dependencies
    
    * removed support of set_chatroom_active_or_inactive from chatroom_manager.py
    
    * removed initialisation of expiry_time in api/collabcard_seen
    
    * removing updation of expire_at in create_chatroom_state_instance in models.py
    
    * removing support of active in process_chatroom method
    
    * removing support of active in fetch_chatroom
    
    * removed support of active in chatroom serializers
    
    * removed show_active support in fetch_chatroom apis
    
    * removing show_active in fetch_chatroom
    
    * resolving issues while removing active chatroom support
    
    Co-authored-by: Deepanshu Saxena <deepanshu.saxena@likeminds.community>

--- project/collabmates_api/chatroom_member/chatroom_member_impl.py
+++ project/collabmates_api/chatroom_member/chatroom_member_impl.py
@@ -698,13 +698,9 @@ class ChatroomMemberHelper:
         chatroom_user_actions['follow_status'] = state_instance.follow_status
         chatroom_user_actions['attending_status'] = state_instance.attending_status
         chatroom_user_actions['is_guest'] = state_instance.is_guest
-        chatroom_user_actions['active'] = False
         chatroom_user_actions['is_tagged'] = state_instance.is_tagged
         chatroom_user_actions['attended'] = state_instance.attended
         expiry_time = state_instance.expiry_time
-
-        if not expiry_time or expiry_time >= TimeUtilities.current_time_in_sec():
-            chatroom_user_actions['active'] = True
 
         return chatroom_user_actions
 
