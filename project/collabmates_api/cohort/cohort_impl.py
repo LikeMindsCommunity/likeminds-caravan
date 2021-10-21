0e9494fdc000c1e481ee757e54f8bdcf789c5c85
Deepanshu Saxena <84494479+Deepanshu-LikeMinds@users.noreply.github.com>
2021-10-21 10:23:35 +0530

    fetching cohorts having no member (#4151)

--- project/collabmates_api/cohort/cohort_impl.py
+++ project/collabmates_api/cohort/cohort_impl.py
@@ -225,11 +225,20 @@ class CohortImpl(CohortManager):
         if not is_cm:
             return {'success': False, 'error_message': "User doesn’t have the ability to fetch cohort"}
 
-        cohort_list = list(ModelUtilities.get_model_filter(CohortMember, {'cohort__community_id': community_id})
-                           .values('cohort').annotate(total_members=Count('cohort'), name=F('cohort__name'))
-                           .order_by('cohort_id').values('cohort_id', 'name', 'total_members'))
+        cohort_context_list = []
 
-        return {'success': True, 'cohorts': cohort_list}
+        cohort_list = ModelUtilities.get_model_filter(Cohort, {'community_id': community_id})
+
+        for cohort in cohort_list:
+
+            cohort_context = {
+                'cohort_id': cohort.id,
+                'name': cohort.name,
+                'total_members': ModelUtilities.get_model_filter(CohortMember, {'cohort_id': cohort.id}).count()
+            }
+            cohort_context_list.append(cohort_context)
+
+        return {'success': True, 'cohorts': cohort_context_list}
 
     def remove_member_from_cohort(self, request_body):
         user_id = request_body.get('user_id', "")
