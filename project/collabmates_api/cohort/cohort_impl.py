7acfd1fc7d515f49a91d2266ad2eb5467ba3c57d
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2021-11-16 21:07:17 +0530

    added logger in self.update_cohort

--- project/collabmates_api/cohort/cohort_impl.py
+++ project/collabmates_api/cohort/cohort_impl.py
@@ -469,7 +469,10 @@ class CohortImpl(CohortManager):
                         'type_id': subscription['plan'].get('plan_id')
                     }
 
-                    self.update_cohort(request_body)
+                    update_cohort_response = self.update_cohort(request_body)
+
+                    if update_cohort_response.get('error_message'):
+                        error_logger.error(update_cohort_response)
 
         return {'success': True}
 
