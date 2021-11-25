012907fa1fdd6b05a1f5a2139f43fc1c217d34fb
Naman Jain <naman.jain@likeminds.community>
2021-11-25 20:18:01 +0530

    added new template names for whatsapp, fixed wrong url issue

--- project/utility/url_utilities.py
+++ project/utility/url_utilities.py
@@ -6,3 +6,14 @@ class UrlUtilities:
     @staticmethod
     def encode_query_url(query):
         return parse.urlencode(query, quote_via=parse.quote)
+
+    @staticmethod
+    def extract_part_from_url(url, part_name, init_slash_off=False):
+        """Extract scheme, netloc, path, query or fragment from url"""
+        parsed = parse.urlsplit(url)
+        parsed = getattr(parsed, part_name)
+
+        if init_slash_off:
+            parsed = parsed[1:]
+
+        return parsed
