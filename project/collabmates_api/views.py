7ba2e93ba3f2463cba61ee3397cfa8dd3e3c871e
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2022-01-06 14:58:00 +0530

    Added new key for testing only. (#4393)

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -8926,6 +8926,7 @@ def config(request):
 
     access = is_user_community_part(user_instance.id)
     context['access'] = access
+    context['community_id'] = 49992
 
     context['survey_seen'] = False
 
