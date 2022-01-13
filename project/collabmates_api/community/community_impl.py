a3da9f5f5085d9d4f6799595d648a67f63a9d4ac
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2022-01-13 18:27:54 +0530

    LM-6115 Added version code support (#4422)

--- project/collabmates_api/community/community_impl.py
+++ project/collabmates_api/community/community_impl.py
@@ -1963,7 +1963,7 @@ class CommunityHelper:
         return join_link_valid
 
     @staticmethod
-    def fetch_community_for_aj(aj, user_id):
+    def fetch_community_for_aj(aj, user_id, platform_code, version_code):
         res = {
             'success': False
         }
@@ -1972,8 +1972,9 @@ class CommunityHelper:
 
         if is_aj_present:
             aj_instance = is_aj_present[0]
+            is_cm_onboarding_enabled = cm_onboarding_version_check(platform_code, version_code)
 
-            if aj_instance.community.is_paid:
+            if is_cm_onboarding_enabled and aj_instance.community.is_paid:
                 user_instance = ModelUtilities.get_model_instance_or_none(User, user_id)
 
                 if not user_instance:
