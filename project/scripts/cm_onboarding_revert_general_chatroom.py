78c8dfc376c9a7df111f408d91d94126fa72c87f
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2022-01-14 18:18:30 +0530

    [LM-6128] Revert general chatroom from prod (#4429)
    * Added revert code for general chatrooms.
    
    * Updating updated_at for sync_chatrooms

--- project/scripts/cm_onboarding_revert_general_chatroom.py
+++ project/scripts/cm_onboarding_revert_general_chatroom.py
@@ -1,6 +1,7 @@
-from togther.models import ModelUtilities, Community, Collabcard
+from togther.models import ModelUtilities, Community, Collabcard, collabcardState
 from utility.states import card_types
 from collabmates_api.static_text import GENERAL_CHAT_TITLE_TEXT, GENERAL_CHAT_HEADER
+from utility.time_utilities import TimeUtilities
 
 
 def remove_general_chatroom_in_previous_communitites():
@@ -34,4 +35,16 @@ def remove_general_chatroom_in_previous_communitites():
                 'created_at__gte': 1642096800000  # As this was created at 13-01-2022 23:30
             }
 
-            ModelUtilities.model_update(Collabcard, filter_dict, {'is_deleted': True})
+            card_filter = ModelUtilities.get_model_filter(Collabcard, filter_dict)
+
+            if not card_filter:
+                continue
+
+            card_instance = card_filter[0].id
+
+            ModelUtilities.model_update(Collabcard, {'id': card_instance.id},
+                                        {'is_deleted': True,
+                                         'updated_at': TimeUtilities.current_time_in_milliseconds()})
+
+            ModelUtilities.model_update(collabcardState, {'card': card_instance},
+                                        {'updated_at': TimeUtilities.current_time_in_sec()})
