0aa4a4df60d304f7e0d5b58d97cf77fb227ed3f8
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2022-01-14 17:28:32 +0530

    [LM-6128] Added revert code for general chatrooms. (#4427)

---
+++ project/scripts/cm_onboarding_revert_general_chatroom.py
@@ -0,0 +1,37 @@
+from togther.models import ModelUtilities, Community, Collabcard
+from utility.states import card_types
+from collabmates_api.static_text import GENERAL_CHAT_TITLE_TEXT, GENERAL_CHAT_HEADER
+
+
+def remove_general_chatroom_in_previous_communitites():
+
+    all_communities_filter = ModelUtilities.get_model_filter(Community, {})
+
+    communities_count = len(all_communities_filter)
+
+    for community_instance in all_communities_filter:
+
+        print("Communities left", communities_count)
+        communities_count -= 1
+
+        filter_dict = {
+            'community': community_instance,
+            'type': card_types.CARD_NORMAL,
+            'include_members_later': True,
+            'auto_follow_done': True,
+            'created_at__lt': 1642096800000
+        }
+
+        if ModelUtilities.is_model_filter_exists(Collabcard, filter_dict):
+
+            filter_dict = {
+                'community': community_instance,
+                'type': card_types.CARD_NORMAL,
+                'include_members_later': True,
+                'auto_follow_done': True,
+                'title': GENERAL_CHAT_TITLE_TEXT,
+                'header': GENERAL_CHAT_HEADER,
+                'created_at__gte': 1642096800000  # As this was created at 13-01-2022 23:30
+            }
+
+            ModelUtilities.model_update(Collabcard, filter_dict, {'is_deleted': True})
