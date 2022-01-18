3051146b99e9bd3ec42c75e260d58c303570ff14
Deepanshu Saxena <84494479+Deepanshu-LikeMinds@users.noreply.github.com>
2022-01-18 14:50:57 +0530

    LM-6155 Jenkins install requirements before taking a pull from the branch (#4438)
    * installing dependencies after pulling latest branch on beta server
    
    * installing dependencies after pulling latest branch from origin on production server

--- project/deployment/deploy-on-beta.sh
+++ project/deployment/deploy-on-beta.sh
@@ -1,13 +1,9 @@
 cd likeminds
 source bin/activate
 cd project
-pip3 install -r requirements.txt
-deactivate
 git checkout development
 git pull
-cd ../
-source bin/activate
-cd project
+pip3 install -r requirements.txt
 DJANGO_SETTINGS_MODULE=project.settings.beta python3 manage.py makemigrations
 DJANGO_SETTINGS_MODULE=project.settings.beta python3 manage.py migrate
 deactivate
