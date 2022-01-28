76ef86184edd7aa58e8a349b39c27c615b2c6f30
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2022-01-28 21:45:14 +0530

    changed from name as per previous flow.

--- project/collabmates_api/community/community_impl.py
+++ project/collabmates_api/community/community_impl.py
@@ -2306,7 +2306,8 @@ class CommunityHelper:
         send_email_response = MailWrapper.send_email_with_custom_from_email.delay(subject=mail_subject,
                                                                                   template=mail_template,
                                                                                   to_mails_list=valid_email_ids_list,
-                                                                                  reply_to=INVITE_MEMBER_REPLY_EMAIL)
+                                                                                  reply_to=INVITE_MEMBER_REPLY_EMAIL,
+                                                                                  from_name=INVITE_MEMBER_FROM_MAIL_NAME)
         return send_email_response
 
     @staticmethod
