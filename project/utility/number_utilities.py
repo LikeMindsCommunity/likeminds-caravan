1992432a65c1b7d0e1cf865b6558ce32ebc22d2f
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2022-02-04 12:43:53 +0530

    added a method convert list to integer list

--- project/utility/number_utilities.py
+++ project/utility/number_utilities.py
@@ -12,3 +12,17 @@ class NumberUtilities:
         map_object = map(int, string.split(delimiter))
 
         return list(map_object)
+
+    @staticmethod
+    def convert_list_to_integer_list_or_raise_exception(list_to_convert: list):
+
+        if not isinstance(list_to_convert, list):
+            print(f"Expected list but got {type(list_to_convert)}")
+            raise Exception(f"Expected list but got {type(list_to_convert)}")
+
+        integer_list = []
+
+        for value in list_to_convert:
+            integer_list.append(int(value))
+
+        return integer_list
