121e224eaf3e40a738b43ee9cf770a8de31fd05c
Mahir-G2 <76210826+Mahir-G2@users.noreply.github.com>
2022-02-11 16:42:27 +0530

    changes related to hubspot updates (#4506)

--- project/external_services/airtable/constants.py
+++ project/external_services/airtable/constants.py
@@ -1,5 +1,13 @@
 from django.conf import settings
+from utility.states import airtable_webhook_types
 
 AIRTABLE_WEBHOOK_PROD = 'https://hooks.airtable.com/workflows/v1/genericWebhook/appb6t4SAoDF2gdpN/wfl2nAPWkipseGdJR/wtrtlsxBhIBMvm9zK'
 AIRTABLE_WEBHOOK_BETA = 'https://hooks.airtable.com/workflows/v1/genericWebhook/appb6t4SAoDF2gdpN/wfl9ROVu0hA6shTZ9/wtrAPGD9XHMqJXPP2'
 JOIN_DATA_WEBHOOK = AIRTABLE_WEBHOOK_BETA if settings.IS_BETA else AIRTABLE_WEBHOOK_PROD
+
+APPROVE_REJECT_WEBHOOK = 'https://hooks.airtable.com/workflows/v1/genericWebhook/appb6t4SAoDF2gdpN/wflalG5aKXPDW0MIL/wtrZJQAmxxjASFdBV'
+
+WEBHOOK_TYPES = {
+    airtable_webhook_types.JOIN_COMMUNITY: JOIN_DATA_WEBHOOK,
+    airtable_webhook_types.APPROVE_REQUEST: APPROVE_REJECT_WEBHOOK
+}
