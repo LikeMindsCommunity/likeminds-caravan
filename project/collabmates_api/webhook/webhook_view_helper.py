f465958fe87d29b21dc9e9711c17e78853b03d13
Mahir-G2 <76210826+Mahir-G2@users.noreply.github.com>
2022-02-28 19:39:17 +0530

    [LM-6377] Webhooks (#4554)
    * CRUD operations on webhooks
    
    * webhook changes
    
    * fixes after pr review
    
    * changes after pr review
    
    * changes after pr review
    
    * changes after dev testing
    
    * changes

--- project/collabmates_api/webhook/webhook_view_helper.py
+++ project/collabmates_api/webhook/webhook_view_helper.py
@@ -5,24 +5,36 @@ from utility.response_utilities import ResponseUtilities
 class WebhookViewHelper:
 
     @staticmethod
-    def fetch_webhook_body_validator(request_body, member_id):
+    def basic_body_validator(request_body, member_id):
 
         if not request_body:
             return ResponseUtilities.get_inner_error_context('invalid request body')
 
-        if 'community_id' not in request_body:
-            return ResponseUtilities.get_inner_error_context('send community_id in body')
-
         if not member_id:
             return ResponseUtilities.get_inner_error_context('send member_id in headers')
+
+        return None
+
+    @staticmethod
+    def fetch_webhooks_body_validator(request_body, member_id):
+
+        basic_validator = WebhookViewHelper.basic_body_validator(request_body, member_id)
+
+        if basic_validator:
+            return basic_validator
+
+        if 'community_id' not in request_body:
+            return ResponseUtilities.get_inner_error_context('send community_id in body')
 
         return request_body
 
     @staticmethod
     def add_webhook_body_validator(request_body, member_id):
 
-        if not request_body:
-            return ResponseUtilities.get_inner_error_context('invalid request body')
+        basic_validator = WebhookViewHelper.basic_body_validator(request_body, member_id)
+
+        if basic_validator:
+            return basic_validator
 
         if 'community_id' not in request_body:
             return ResponseUtilities.get_inner_error_context('send community_id in body')
@@ -36,32 +48,37 @@ class WebhookViewHelper:
         if 'url' not in request_body:
             return ResponseUtilities.get_inner_error_context('send url in body')
 
-        if not member_id:
-            return ResponseUtilities.get_inner_error_context('send member_id in headers')
+        return request_body
+
+    @staticmethod
+    def fetch_webhook_body_validator(request_body, member_id):
+
+        basic_validator = WebhookViewHelper.basic_body_validator(request_body, member_id)
+
+        if basic_validator:
+            return basic_validator
 
         return request_body
 
     @staticmethod
     def update_webhook_body_validator(request_body, member_id):
 
-        if not request_body:
-            return ResponseUtilities.get_inner_error_context('invalid request body')
+        basic_validator = WebhookViewHelper.basic_body_validator(request_body, member_id)
+
+        if basic_validator:
+            return basic_validator
 
         if 'url' not in request_body:
             return ResponseUtilities.get_inner_error_context('send url in body')
-
-        if not member_id:
-            return ResponseUtilities.get_inner_error_context('send member_id in headers')
 
         return request_body
 
     @staticmethod
     def delete_webhook_body_validator(request_body, member_id):
 
-        if not request_body:
-            return ResponseUtilities.get_inner_error_context('invalid request body')
+        basic_validator = WebhookViewHelper.basic_body_validator(request_body, member_id)
 
-        if not member_id:
-            return ResponseUtilities.get_inner_error_context('send member_id in headers')
+        if basic_validator:
+            return basic_validator
 
         return request_body
--- project/collabmates_api/webhook/webhook_views.py
+++ project/collabmates_api/webhook/webhook_views.py
@@ -14,7 +14,7 @@ class WebhooksView(APIView):
 
         request_body = RequestUtilities.load_request_body(request)
         member_id = RequestUtilities.get_member_id_from_headers(request)
-        validated_request_body = WebhookViewHelper.fetch_webhook_body_validator(request_body, member_id)
+        validated_request_body = WebhookViewHelper.fetch_webhooks_body_validator(request_body, member_id)
 
         if 'error_message' in validated_request_body:
             context = ResponseUtilities.get_view_impl_error_context(validated_request_body['error_message'],
@@ -23,7 +23,7 @@ class WebhooksView(APIView):
 
         webhook_manager = WebhookImpl(member_id=member_id,
                                       community_id=validated_request_body.get('community_id'))
-        response_data = webhook_manager.fetch_webhook()
+        response_data = webhook_manager.fetch_webhooks()
 
         if 'error_message' in response_data:
             context = ResponseUtilities.get_view_impl_error_context(response_data['error_message'],
@@ -77,7 +77,6 @@ class WebhookView(APIView):
             return JsonResponse(context['data'], status=context['status'])
 
         webhook_manager = WebhookImpl(member_id=member_id,
-                                      community_id=validated_request_body.get('community_id'),
                                       webhook_id=webhook_id)
         response_data = webhook_manager.fetch_webhook()
 
@@ -88,34 +87,6 @@ class WebhookView(APIView):
 
         return JsonResponse(
             {'success': True, 'webhooks': response_data['webhooks']},
-            status=status_codes.HTTP_200_OK
-        )
-
-    def post(self, request, webhook_id):
-
-        request_body = RequestUtilities.load_request_body(request)
-        member_id = RequestUtilities.get_member_id_from_headers(request)
-        validated_request_body = WebhookViewHelper.add_webhook_body_validator(request_body, member_id)
-
-        if 'error_message' in validated_request_body:
-            context = ResponseUtilities.get_view_impl_error_context(validated_request_body['error_message'],
-                                                                    status_codes.HTTP_400_BAD_REQUEST)
-            return JsonResponse(context['data'], status=context['status'])
-
-        webhook_manager = WebhookImpl(member_id=member_id,
-                                      community_id=validated_request_body.get('community_id'),
-                                      webhook_id=webhook_id,
-                                      webhook_type=validated_request_body.get('webhook_type'),
-                                      url=validated_request_body.get('url'))
-        response_data = webhook_manager.add_webhook()
-
-        if 'error_message' in response_data:
-            context = ResponseUtilities.get_view_impl_error_context(response_data['error_message'],
-                                                                    response_data['status'])
-            return JsonResponse(context['data'], status=context['status'])
-
-        return JsonResponse(
-            {'success': True},
             status=status_codes.HTTP_200_OK
         )
 
