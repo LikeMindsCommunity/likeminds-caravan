a3c8965cead2d1d95f94f7f341b85b2052ba2cfe
Naman Jain <89894541+naman-likeminds@users.noreply.github.com>
2022-03-01 14:21:52 +0530

    [LM-6464] self noti for CMs in events (#4555)
    * stop sending registration noti to self for CM
    
    * removing user after non-member access event filter
    
    * resolved int issue

--- project/collabmates_api/chatroom/chatroom_impl.py
+++ project/collabmates_api/chatroom/chatroom_impl.py
@@ -2482,7 +2482,7 @@ class ChatroomImpl(ChatroomManager):
 
             payload_for_email_comms = {
                 'chatroom': chatroom_instance.id,
-                'user': member_id
+                'user': int(member_id)
             }
 
             send_email_notification_for_event_type.delay(payload_for_email_comms, EVENT_TYPE.POST_EVENT_ATTACHMENTS)
@@ -2598,7 +2598,7 @@ class ChatroomImpl(ChatroomManager):
 
                 payload_for_email_comms = {
                     'chatroom': event_obj.id,
-                    'user': member_id
+                    'user': int(member_id)
                 }
 
                 send_email_notification_for_event_type.delay(payload_for_email_comms, EVENT_TYPE.POST_EVENT_ATTACHMENTS)
