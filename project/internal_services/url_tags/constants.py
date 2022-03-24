a8d849a58f8c2de43ae76be1ec2d26ec910fcf74
yadavKetan <73521713+yadavKetan@users.noreply.github.com>
2022-03-24 18:53:10 +0530

    Add user-agent request header (#4590)

---
+++ project/internal_services/url_tags/constants.py
@@ -0,0 +1,6 @@
+REQUEST_TIMEOUT_SECONDS: int = 2
+URL_HIT_RETRIES: int = 3
+RETRY_BACKOFF_FACTOR: float = 0.1
+FORCE_STATUS_LIST: list = [500, 502, 503, 504]
+BROWSER_USER_AGENT: str = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                     'Chrome/50.0.2661.102 Safari/537.36 '
