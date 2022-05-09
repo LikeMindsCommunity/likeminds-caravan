eaa36896e09d07897efc60d13d0eae7752ddf910
Mahir-G2 <76210826+Mahir-G2@users.noreply.github.com>
2022-05-09 12:38:51 +0530

    [LM - 6892] Create and Initiate Project validator fix (#4655)
    * initial changes related to create and initiate project apis
    
    * changes in create project api
    
    * fixes after pr review
    
    * changes related to initiate sdk api
    
    * fixes in initiate project api
    
    * fix after testing
    
    * fix in create sdk project api validator

--- project/collabmates_api/sdk/sdk_view_helper.py
+++ project/collabmates_api/sdk/sdk_view_helper.py
@@ -21,7 +21,7 @@ class SdkViewHelper:
         if 'brand_color' not in request_body:
             return ResponseUtilities.get_inner_error_context('send brand_color in body')
 
-        if 'platform' in request_body and not isinstance(request_body['platform'], list):
+        if 'platform' in request_body and request_body['platform'] and not isinstance(request_body['platform'], list):
             return ResponseUtilities.get_inner_error_context('platform object should be a list')
 
         return request_body
