62f5a46c05f60f356f34cb059a54bb6c5b4e57a2
Mahir-G2 <76210826+Mahir-G2@users.noreply.github.com>
2022-05-24 17:53:51 +0530

    Revert "[LM-6725] Notification Customisation (#4602)" (#4673)
    This reverts commit 006a86cac72f18aea60d9035748072ae36a26a64.

--- project/collabmates_api/notifications/tasks_impl.py
+++ project/collabmates_api/notifications/tasks_impl.py
@@ -1159,17 +1159,6 @@ class TasksHelper:
         return calendar_id
 
     @staticmethod
-    def add_community_info_to_notification_payload(message: dict, community_id: int) -> dict:
-
-        community_instance = ModelUtilities.get_model_instance_or_none(Community, community_id)
-
-        if community_instance:
-            message['payload']['community_name'] = community_instance.name
-            message['payload']['community_logo'] = community_instance.thumbnail
-
-        return message
-
-    @staticmethod
     def fetch_event_banner(event):
         filter_dict = {
             'collabcard_id': event,
