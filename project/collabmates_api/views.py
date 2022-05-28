12fa2cbfcfc3d44f6f9d485d985315041cf69c21
ankit-grg <ankit.garg@likeminds.community>
2022-05-28 14:22:03 +0530

    Added support of x-api-key in api/update_member_rights API.

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -11134,7 +11134,8 @@ def update_community_member_rights(request):
     """ function to remove a communtiy manager as manager """
 
     if request.method == 'GET':
-        return JsonResponse({'success': False, 'error_message': 'Change HTTP method to POST'})
+        return JsonResponse({'success': False, 'error_message': 'Change HTTP method to POST'},
+                            status=status_codes.HTTP_405_METHOD_NOT_ALLOWED)
 
     current_user_id = get_member_id_from_headers(request)
     req_body = json.loads(request.body)
@@ -11142,20 +11143,40 @@ def update_community_member_rights(request):
     community_id = req_body['community_id'] if "community_id" in req_body else None
     selected_rights = req_body['rights'] if "rights" in req_body else []
     custom_title = req_body['custom_title'] if "custom_title" in req_body else None
+    api_key = RequestUtilities.get_api_key_from_headers(request)
+
+    community_id = community_id if community_id else api_key
 
     if not current_user_id:
         context = get_error_context(False, "send member_id in headers")
-        return JsonResponse(context)
+        return JsonResponse(context, status=status_codes.HTTP_400_BAD_REQUEST)
+
     if not user_id:
         context = get_error_context(False, "send user_id in body")
-        return JsonResponse(context)
-    if not community_id:
-        context = get_error_context(False, "send community_id in body")
-        return JsonResponse(context)
+        return JsonResponse(context, status=status_codes.HTTP_400_BAD_REQUEST)
 
-    community_instance = Community.objects.get(pk=community_id)
-    current_user_instance = User.objects.get(pk=current_user_id)
-    user_instance = User.objects.get(pk=user_id)
+    if not community_id:
+        context = get_error_context(False, "Invalid API key/community ID")
+        return JsonResponse(context, status=status_codes.HTTP_400_BAD_REQUEST)
+
+    community_instance = SdkClient.get_community_instance_or_none(community_id)
+
+    if not community_instance:
+        context = get_error_context(False, "Invalid API key/community ID")
+        return JsonResponse(context, status=status_codes.HTTP_400_BAD_REQUEST)
+
+
+    current_user_instance = ModelUtilities.get_user_instance_or_none(current_user_id)
+
+    if not current_user_instance:
+        context = get_error_context(False, "Invalid x-member-id")
+        return JsonResponse(context, status=status_codes.HTTP_400_BAD_REQUEST)
+
+    user_instance = ModelUtilities.get_user_instance_or_none(user_id)
+
+    if not user_instance:
+        context = get_error_context(False, "Invalid user_id")
+        return JsonResponse(context, status=status_codes.HTTP_400_BAD_REQUEST)
 
     info_logger.info(f"UPDATING_MEMBER_RIGHTS - current user id = {current_user_id}, user id = {user_id}"
                      f", community id = {community_id}")
