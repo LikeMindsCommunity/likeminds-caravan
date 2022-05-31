e328d27b147a3899fe53aae942884445111d520f
Mahir-G2 <mahir.gupta@likeminds.community>
2022-05-31 17:23:33 +0530

    updated keys

--- project/collabmates_api/sdk/sdk_view_helper.py
+++ project/collabmates_api/sdk/sdk_view_helper.py
@@ -71,6 +71,16 @@ class SdkViewHelper:
         if not request_body:
             return ResponseUtilities.get_inner_error_context('Invalid request body')
 
+        req_body = request_body.copy()
+
+        if 'name' in req_body and req_body['name']:
+            req_body['community_name'] = req_body['name']
+            del req_body['name']
+
+        if 'headline' in req_body and req_body['headline']:
+            req_body['purpose'] = req_body['headline']
+            del req_body['headline']
+
         if not member_id:
             return ResponseUtilities.get_inner_error_context('Send member_id in headers')
 
@@ -85,7 +95,7 @@ class SdkViewHelper:
         if 'platform' in request_body and request_body['platform'] and not isinstance(request_body['platform'], list):
             return ResponseUtilities.get_inner_error_context('platform object should be a list')
 
-        return {}
+        return {'req_body': req_body}
 
     @staticmethod
     def delete_sdk_project_validator(member_id):
