6ef17c3b12b78bf4c24a5e6ee9d029c7d1c13aed
ankit-grg <ankit.garg@likeminds.community>
2022-06-09 16:37:45 +0530

    Added defensive check for open chatroom.

--- project/collabmates_api/chatroom/chatroom_view_helper.py
+++ project/collabmates_api/chatroom/chatroom_view_helper.py
@@ -2,6 +2,7 @@ from utility.response_utilities import ResponseUtilities
 from togther.models import (ModelUtilities, Members, Collabcard)
 from collabmates_api.sdk.models import SdkClient
 from rest_framework import status as status_codes
+from utility.states import (member_states)
 
 
 class ChatroomViewHelper:
@@ -107,3 +108,35 @@ class ChatroomViewHelper:
 
         return {'user_instance': user_instance, 'card_instance': card_instance,
                 'secret_chatroom_participants': secret_chatroom_participants}
+
+    @staticmethod
+    def validate_add_members_to_open_chatroom(user_id, chatroom_id, chatroom_participants):
+        user_instance = ModelUtilities.get_user_instance_or_none(user_id)
+
+        if not user_instance:
+            return ResponseUtilities.get_inner_error_context("In-valid user id")
+
+        card_instance = ModelUtilities.get_model_instance_or_none(Collabcard, chatroom_id)
+
+        if not card_instance:
+            return ResponseUtilities.get_inner_error_context("In-valid chatroom id")
+
+        if card_instance.is_secret:
+            return ResponseUtilities.get_inner_error_context("Chatroom is secret!")
+
+        if not chatroom_participants:
+            return ResponseUtilities.get_inner_error_context("Invalid Chatroom participants")
+
+        member_filter = ModelUtilities.get_model_filter(Members, {'community_id': card_instance.community,
+                                                                  'member_id': user_instance})
+
+        if not member_filter:
+            return ResponseUtilities.get_inner_error_context("User is not a member of community")
+
+        member_instance = member_filter[0]
+        is_cm = member_instance.state == member_states.ADMIN
+
+        if not is_cm:
+            return ResponseUtilities.get_inner_error_context("User doesn't have the ability to perform this operation")
+
+        return {'user_instance': user_instance, 'card_instance': card_instance}
