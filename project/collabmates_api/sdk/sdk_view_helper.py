83c0aa76aa631df2230eb05f171e03cafffdbed1
Mahir-G2 <mahir.gupta@likeminds.community>
2022-07-17 16:45:19 +0530

    onboarding screen apis

--- project/collabmates_api/sdk/sdk_view_helper.py
+++ project/collabmates_api/sdk/sdk_view_helper.py
@@ -1,6 +1,7 @@
 from utility.response_utilities import ResponseUtilities
 from togther.models import ModelUtilities
 from utility.states import (login_types)
+from .models import SdkClient, SdkOnboardingScreen
 
 
 class SdkViewHelper:
@@ -142,3 +143,118 @@ class SdkViewHelper:
             join_req_body['image_url'] = request_body.get('image_url')
 
         return {'login_req_body': login_req_body, 'join_req_body': join_req_body}
+
+    @staticmethod
+    def fetch_onboarding_screens_validator(request_params, api_key):
+
+        if not api_key:
+            return ResponseUtilities.get_inner_error_context('Send api_key in headers')
+
+        community_instance = SdkClient.get_community_instance_or_none(api_key=api_key)
+
+        if not community_instance:
+            return ResponseUtilities.get_inner_error_context("Invalid API key!")
+
+        filters = {
+            'community': community_instance
+        }
+
+        if request_params.get('screen_id', None):
+            filters['id'] = request_params['screen_id']
+
+        onboarding_screens = ModelUtilities.get_model_filter(SdkOnboardingScreen, filters)
+
+        return {'onboarding_screens': onboarding_screens}
+
+    @staticmethod
+    def create_onboarding_screen_validator(request_body, api_key, member_id):
+
+        if not request_body:
+            return ResponseUtilities.get_inner_error_context('Invalid request body')
+
+        if not request_body.get('index'):
+            return ResponseUtilities.get_inner_error_context('Send valid screen index')
+
+        if not request_body.get('image'):
+            return ResponseUtilities.get_inner_error_context('Send valid image url')
+
+        member_validator = SdkViewHelper._member_id_validator(member_id)
+
+        if 'error_message' in member_validator:
+            return member_validator
+
+        if not api_key:
+            return ResponseUtilities.get_inner_error_context('Send api_key in headers')
+
+        community_instance = SdkClient.get_community_instance_or_none(api_key=api_key)
+
+        if not community_instance:
+            return ResponseUtilities.get_inner_error_context("Invalid API key!")
+
+        return {'user_instance': member_validator.get('user_instance'), 'community_instance': community_instance}
+
+    @staticmethod
+    def edit_onboarding_screen_validator(request_body, api_key, member_id):
+
+        if not request_body:
+            return ResponseUtilities.get_inner_error_context('Invalid request body')
+
+        if not request_body.get('id'):
+            return ResponseUtilities.get_inner_error_context('Send valid id')
+
+        if 'index' in request_body and not request_body.get('index'):
+            return ResponseUtilities.get_inner_error_context('Send valid screen index')
+
+        if 'image' in request_body and not request_body.get('image'):
+            return ResponseUtilities.get_inner_error_context('Send valid image url')
+
+        member_validator = SdkViewHelper._member_id_validator(member_id)
+
+        if 'error_message' in member_validator:
+            return member_validator
+
+        if not api_key:
+            return ResponseUtilities.get_inner_error_context('Send api_key in headers')
+
+        community_instance = SdkClient.get_community_instance_or_none(api_key=api_key)
+
+        if not community_instance:
+            return ResponseUtilities.get_inner_error_context("Invalid API key!")
+
+        screen = ModelUtilities.get_model_instance_or_none(SdkOnboardingScreen, request_body.get('id'))
+
+        if not screen:
+            return ResponseUtilities.get_inner_error_context("Invalid screen id sent")
+
+        return {'user_instance': member_validator.get('user_instance'), 'community_instance': community_instance,
+                'screen_instance': screen}
+
+    @staticmethod
+    def delete_onboarding_screen_validator(request_body, api_key, member_id):
+
+        if not request_body:
+            return ResponseUtilities.get_inner_error_context('Invalid request body')
+
+        if not request_body.get('id'):
+            return ResponseUtilities.get_inner_error_context('Send valid id')
+
+        member_validator = SdkViewHelper._member_id_validator(member_id)
+
+        if 'error_message' in member_validator:
+            return member_validator
+
+        if not api_key:
+            return ResponseUtilities.get_inner_error_context('Send api_key in headers')
+
+        community_instance = SdkClient.get_community_instance_or_none(api_key=api_key)
+
+        if not community_instance:
+            return ResponseUtilities.get_inner_error_context("Invalid API key!")
+
+        screen = ModelUtilities.get_model_instance_or_none(SdkOnboardingScreen, request_body.get('id'))
+
+        if not screen:
+            return ResponseUtilities.get_inner_error_context("Invalid screen id sent")
+
+        return {'user_instance': member_validator.get('user_instance'), 'community_instance': community_instance,
+                'screen_instance': screen}
--- project/collabmates_api/sdk/sdk_views.py
+++ project/collabmates_api/sdk/sdk_views.py
@@ -4,7 +4,6 @@ from rest_framework import status as status_codes
 
 from utility.request_utilities import RequestUtilities
 from utility.response_utilities import ResponseUtilities
-from .sdk_view_helper import SdkViewHelper
 from .sdk_impl import SdkImpl
 
 
@@ -124,5 +123,84 @@ class AuthenticateSdkView(APIView):
             context = ResponseUtilities.get_view_impl_error_context(response_data['error_message'],
                                                                     response_data['status'])
             return JsonResponse(**context)
+
+        return JsonResponse(response_data, status=status_codes.HTTP_200_OK)
+
+
+class OnboardingScreensView(APIView):
+
+    def get(self, request):
+
+        request_params = RequestUtilities.fetch_request_query_params(request)
+        member_id = RequestUtilities.get_member_id_from_headers(request)
+        request_platform = RequestUtilities.get_platform_code(request)
+        version_code = RequestUtilities.get_version_code_from_headers(request)
+        api_key = RequestUtilities.get_api_key_from_headers(request)
+
+        sdk_manager = SdkImpl(member_id=member_id, request_platform=request_platform, version_code=version_code,
+                              api_key=api_key)
+        response_data = sdk_manager.fetch_onboarding_screens(request_params)
+
+        if 'error_message' in response_data:
+            context = ResponseUtilities.get_view_impl_error_context(response_data['error_message'],
+                                                                    response_data['status'])
+            return JsonResponse(context['data'], status=context['status'])
+
+        return JsonResponse(response_data, status=status_codes.HTTP_200_OK)
+
+    def post(self, request):
+
+        request_body = RequestUtilities.load_request_body(request)
+        member_id = RequestUtilities.get_member_id_from_headers(request)
+        request_platform = RequestUtilities.get_platform_code(request)
+        version_code = RequestUtilities.get_version_code_from_headers(request)
+        api_key = RequestUtilities.get_api_key_from_headers(request)
+
+        sdk_manager = SdkImpl(member_id=member_id, request_platform=request_platform, version_code=version_code,
+                              api_key=api_key)
+        response_data = sdk_manager.create_onboarding_screen(request_body)
+
+        if 'error_message' in response_data:
+            context = ResponseUtilities.get_view_impl_error_context(response_data['error_message'],
+                                                                    response_data['status'])
+            return JsonResponse(context['data'], status=context['status'])
+
+        return JsonResponse(response_data, status=status_codes.HTTP_201_CREATED)
+
+    def put(self, request):
+
+        request_body = RequestUtilities.load_request_body(request)
+        member_id = RequestUtilities.get_member_id_from_headers(request)
+        request_platform = RequestUtilities.get_platform_code(request)
+        version_code = RequestUtilities.get_version_code_from_headers(request)
+        api_key = RequestUtilities.get_api_key_from_headers(request)
+
+        sdk_manager = SdkImpl(member_id=member_id, request_platform=request_platform, version_code=version_code,
+                              api_key=api_key)
+        response_data = sdk_manager.edit_onboarding_screen(request_body)
+
+        if 'error_message' in response_data:
+            context = ResponseUtilities.get_view_impl_error_context(response_data['error_message'],
+                                                                    response_data['status'])
+            return JsonResponse(context['data'], status=context['status'])
+
+        return JsonResponse(response_data, status=status_codes.HTTP_200_OK)
+
+    def delete(self, request):
+
+        request_body = RequestUtilities.load_request_body(request)
+        member_id = RequestUtilities.get_member_id_from_headers(request)
+        request_platform = RequestUtilities.get_platform_code(request)
+        version_code = RequestUtilities.get_version_code_from_headers(request)
+        api_key = RequestUtilities.get_api_key_from_headers(request)
+
+        sdk_manager = SdkImpl(member_id=member_id, request_platform=request_platform, version_code=version_code,
+                              api_key=api_key)
+        response_data = sdk_manager.delete_onboarding_screen(request_body)
+
+        if 'error_message' in response_data:
+            context = ResponseUtilities.get_view_impl_error_context(response_data['error_message'],
+                                                                    response_data['status'])
+            return JsonResponse(context['data'], status=context['status'])
 
         return JsonResponse(response_data, status=status_codes.HTTP_200_OK)
