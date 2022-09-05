b084eb8778c02e83daee7949f72c3ef2f4a403f2
ankit-grg <ankit.garg@likeminds.community>
2022-09-05 18:39:30 +0530

    Removed SDK notifications from Mixpanel.

--- project/external_services/mixpanel/mixpanel_impl.py
+++ project/external_services/mixpanel/mixpanel_impl.py
@@ -2,6 +2,8 @@ from .mixpanel_manager import MixpanelManager
 from django.conf import settings
 from mixpanel import Mixpanel
 from .constants import MIXPANEL_NOTIFICATION_TRACKING_EVENT_NAME
+from togther.models import (ModelUtilities)
+from collabmates_api.sdk.models import (SdkClient)
 from external_services.logging.logging_wrapper import LoggingWrapper
 error_logger = LoggingWrapper.get_instance()
 info_logger = LoggingWrapper.get_instance()
@@ -17,6 +19,14 @@ class MixpanelImpl(MixpanelManager):
         self.mixpanel_instance = Mixpanel(token=settings.MIXPANEL_TOKEN)
 
     def track_notification(self, distinct_id, properties) -> None:
+        payload = properties.get('payload')
+        community_id = payload.get('community_id')
+
+        community_instance = ModelUtilities.get_model_filter(SdkClient, {'community': community_id})
+
+        if community_instance:
+            return
+
         try:
             self.mixpanel_instance.track(distinct_id=distinct_id,
                                          event_name=MIXPANEL_NOTIFICATION_TRACKING_EVENT_NAME,
