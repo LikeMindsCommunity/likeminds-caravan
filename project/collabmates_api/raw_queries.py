a28bf4b00eaaff7ca62301288b053592cbf01028
ankit-grg <ankit.garg@likeminds.community>
2022-09-13 12:43:18 +0530

    Users self followed chatroom conversations to be considered.

--- project/collabmates_api/raw_queries.py
+++ project/collabmates_api/raw_queries.py
@@ -229,6 +229,8 @@ def get_followed_chatrooms(user_id,
                                                              conversation_states.CONVERSATION_POLL,
                                                              conversation_states.CONVERSATION_EVENT])
 
+        follow_conversation_state = get_tuple_from_array([conversation_states.CONVERSATION_FOLLOW])
+
         conn = get_connection()
         curr = conn.cursor()
 
@@ -273,27 +275,30 @@ def get_followed_chatrooms(user_id,
                 FROM       togther_conversationengage
                 INNER JOIN (WITH added_row_number AS
                            (
-                                  SELECT ca.created_at,
-                                         ca.id,
-                                         ca.card_id,
-                                         row_number() OVER( partition BY ca.card_id ORDER BY (
-                                         CASE
-                                                WHEN ca.state     IN %s THEN 1
-                                                WHEN ca.state NOT IN %s THEN 2
-                                         END), ca.created_at DESC) AS row_number
-                                  FROM   togther_card_answers      AS ca
-                                  WHERE  ca.card_id IN %s)SELECT   card_id,
-                             id,
-                             created_at
-                    FROM     added_row_number
-                    WHERE    row_number = 1) AS lca
+                                    SELECT   ca.created_at,
+                                             ca.id,
+                                             ca.card_id,
+                                             row_number() OVER( partition BY ca.card_id ORDER BY (
+                                             CASE
+                                                      WHEN ca.state IN %s
+                                                      OR       (
+                                                                        ca.state IN %s
+                                                               AND      ca.user_id = %s) THEN 1
+                                                      WHEN ca.state NOT IN %s THEN 2
+                                             END), ca.created_at DESC) AS row_number
+                                    FROM     togther_card_answers      AS ca
+                                    WHERE    ca.card_id IN %s)SELECT   card_id,
+                           id,
+                           created_at
+                  FROM     added_row_number
+                  WHERE    row_number = 1) AS lca
                   ON       togther_conversationengage.card_id = lca.card_id
                   WHERE    togther_conversationengage.user_id=%s
                   AND      togther_conversationengage.card_id IN %s
                   ORDER BY lca.created_at DESC,
-                           id DESC limit %s offset %s""" % (included_conversation_states, included_conversation_states,
-                                                            card_ids, str(user_id), card_ids, str(limit),
-                                                            str(offset))
+                           id DESC limit %s offset %s""" % (included_conversation_states, follow_conversation_state,
+                                                            user_id, included_conversation_states, card_ids,
+                                                            str(user_id), card_ids, str(limit), str(offset))
 
         curr.execute(sql)
         res = curr.fetchall()
