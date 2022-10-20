1867a37acb2ddecc18436b0e0bd5d1d183bcbf8e
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2022-10-20 18:53:09 +0530

    [LM-7567] Chatrooms not visible on home feed for old members (#4880)
    * Added script for backfilling conversation engage  for followed chatrooms.
    
    * Create conversation engage records for auto_follow_members.
    
    * Resolved minor bug.

--- project/collabmates_api/chatroom/chatroom_impl.py
+++ project/collabmates_api/chatroom/chatroom_impl.py
@@ -3972,6 +3972,8 @@ class ChatroomHelper:
         community_noti_instance = CommunityHelper.fetch_community_noti_settings_instance(community_instance)
         community_current_noti_state = community_noti_instance.noti_state if community_noti_instance else noti_states.ALL_MESSAGES
 
+        auto_follow_members_list = []
+
         for data in member_filter:
             user_instance = data.member_id
 
@@ -4000,13 +4002,16 @@ class ChatroomHelper:
                 if user_instance.id in event_creator_and_community_owner:
                     community_admins_list.append(user_instance.id)
 
+                if follow_status:
+                    auto_follow_members_list.append(user_instance.id)
+
         payload_for_calendar_invite = {
             'chatroom': card_instance.id
         }
 
         send_calender_invite_for_event_type.delay(payload_for_calendar_invite, EVENT_TYPE.REGISTRATION,
-                                                send_to_members=False, user_list=event_creator_and_community_owner,
-                                                calendar_invite_type=CALENDAR_INVITE_TYPE.NEW_CALENDAR_CREATION)
+                                                  send_to_members=False, user_list=event_creator_and_community_owner,
+                                                  calendar_invite_type=CALENDAR_INVITE_TYPE.NEW_CALENDAR_CREATION)
 
         ModelUtilities.bulk_create_instances(collabcardState, bulk_create_list)
         ChatroomHelper.set_chatroom_participants_created_key_in_cache(card_instance.id, True)
@@ -4018,13 +4023,19 @@ class ChatroomHelper:
                 "status": True
             })
 
+        card_engagement_user_list = []
+
         if chatroom_participants_list:
-            ChatroomHelper.create_card_engagements_for_home_screen_for_auto_follow_all_members_with_user_list(
-                card_instance.id, chatroom_participants_list)
+            card_engagement_user_list = chatroom_participants_list
 
         if card_instance.type in [card_types.CARD_EVENT, card_types.CARD_PUBLIC_EVENT]:
-            ChatroomHelper.create_card_engagements_for_home_screen_for_auto_follow_all_members_with_user_list(
-                card_instance.id, community_admins_list)
+            card_engagement_user_list = community_admins_list
+
+        if auto_follow_members_list:
+            card_engagement_user_list = auto_follow_members_list
+
+        ChatroomHelper.create_card_engagements_for_home_screen_for_auto_follow_all_members_with_user_list(
+            card_instance.id, card_engagement_user_list)
 
     @staticmethod
     def update_unseen_count_for_homescreen_communitites(card_instance, community_instance):
