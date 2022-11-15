1d22105fd509825a05c16073b8b730c2a1d26a95
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2022-11-15 13:10:19 +0530

    Revert "[LM-6662] Unsubscribe emails (#4906)"
    This reverts commit 9a5e4bd1aea5e63b237ba6343e502c142abd7b32.

--- project/collabmates_api/notification.py
+++ project/collabmates_api/notification.py
@@ -845,8 +845,7 @@ def schedule_poll_end_notification(card_id):
         celery_beat_task.terminate_task(task_name)
 
         args = [card_id, task_name]
-        # date_time = poll_end_time + SIX_HOURS_IN_SECONDS
-        date_time = poll_end_time + 600
+        date_time = poll_end_time + SIX_HOURS_IN_SECONDS
         celery_beat_task.create_dynamic_clery_task(args=args, kwargs={}, task_name=task_name, task_path=task_path,
                                                    date_time=date_time, interval=False, crontab=True)
 
