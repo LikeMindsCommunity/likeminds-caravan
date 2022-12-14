ef28346cf248748a20b152f989b2974ed3334527
ankit-grg <ankit.garg@likeminds.community>
2022-12-14 11:41:36 +0530

    Removed defensive checks altogether from api/cohort/update API.

--- project/collabmates_api/cohort/cohort_impl.py
+++ project/collabmates_api/cohort/cohort_impl.py
@@ -1293,27 +1293,14 @@ class CohortHelper:
 
         member_instance = None
 
-        is_sdk_community = SdkClient.is_sdk_community(community_instance.id)
-
-        if is_sdk_community and cohort_instance and not community_instance:
+        if cohort_instance and not community_instance:
             community_instance = cohort_instance.community
-
-        if is_sdk_community and cohort_instance.community != community_instance:
-            return ResponseUtilities.get_inner_error_context("Cohort is of different community!")
 
         member_filter = ModelUtilities.get_model_filter(Members, {'community_id': community_instance,
                                                                   'member_id': user_instance})
 
         if member_filter:
             member_instance = member_filter[0]
-
-        if is_sdk_community and member_ids:
-            members_ids_list = list(ModelUtilities.get_model_filter(
-                Members, {'community_id': community_instance,
-                          'member_id__in': member_ids}).values_list('member_id_id', flat=True))
-
-            if len(set(member_ids) - set(members_ids_list)):
-                return ResponseUtilities.get_inner_error_context("Member IDs are not part of community!")
 
         return {
             'user_instance': user_instance,
