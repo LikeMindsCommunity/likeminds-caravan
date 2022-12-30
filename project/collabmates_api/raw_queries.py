3bb49c143e38e006224bbbb62f6aad346c01c83b
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2022-12-30 15:00:18 +0530

    [LM-7758] Remove guest user chatrooms from dm chatroom list (#4963)
    * Removed guest DM chatrooms from chatrooms list.
    
    * Improved some time of serialising DM chatrooms.
    
    * Improved some time of serialising DM chatrooms part 2.
    
    * Updated raw query for getting dm chatrooms for non-guest members.
    
    * Updated raw query for getting dm chatrooms for non-guest members.
    
    * Updated raw query for getting dm chatrooms for non-guest members.

--- project/collabmates_api/raw_queries.py
+++ project/collabmates_api/raw_queries.py
@@ -2526,23 +2526,26 @@ def get_dm_chatrooms_of_user(user_id, community_id):
         else:
             community_id = "=" + str(community_id)
 
-        sql = """SELECT CS.card_id, CS.id
-                   FROM   togther_collabcardstate AS CS
-                   WHERE  CS.follow_status = true
-                          AND CS.remove_id IS NULL
-                          AND CS.secret_chatroom_left = false
-                          AND CS.user_id = %s
-                          AND card_id IN (SELECT id
-                                          FROM   togther_collabcard
-                                                 AS
-                                                 CCRD
-                                          WHERE
-                              CCRD.community_id %s
-                              AND is_private = true
-                              AND CCRD.type = 10
-                              AND ( CCRD.user_id = %s OR
-                              CCRD.chatroom_with_user_id = %s ))
-        """ % (str(user_id), str(community_id), str(user_id), str(user_id))
+        non_guest_user_query = get_user_ids_based_on_guest_filter(is_guest=False, only_sql_query=True)
+
+        sql = """
+                SELECT cs.card_id,
+                       cs.id
+                FROM   togther_collabcardstate AS cs
+                       inner join togther_collabcard AS ccrd
+                               ON cs.card_id = CCRD.id
+                WHERE  ccrd.community_id %s
+                       AND cs.follow_status = TRUE
+                       AND cs.remove_id IS NULL
+                       AND cs.secret_chatroom_left = FALSE
+                       AND cs.user_id = %s
+                       AND ccrd.is_private = TRUE
+                       AND ccrd.TYPE = 10
+                       AND ( ccrd.user_id = %s
+                              OR ccrd.chatroom_with_user_id = %s )
+                       AND ( ccrd.user_id IN ( %s )
+                             AND ccrd.chatroom_with_user_id IN ( %s ) ) 
+        """ % (str(community_id), str(user_id), str(user_id), str(user_id), non_guest_user_query, non_guest_user_query)
 
         curr.execute(sql)
         card_list = curr.fetchall()
@@ -3066,6 +3069,29 @@ def fetch_user_communities_sorted_by_order_time(user_id, community_id=None):
         curr.close()
 
         return [data[0] for data in card_list]
+
+    except (Exception, psycopg2.Error) as error:
+        error_logger.error("Error while connecting to PostgreSQL %s ", error)
+
+
+def get_user_ids_based_on_guest_filter(is_guest=False, only_sql_query=False):
+    try:
+        sql = """
+                SELECT   user_id_id
+                FROM     togther_userinfo
+                WHERE    is_guest = %s""" % is_guest
+
+        if only_sql_query:
+            return sql
+
+        conn = get_connection()
+        curr = conn.cursor()
+
+        curr.execute(sql)
+        user_list = curr.fetchall()
+        curr.close()
+
+        return [data[0] for data in user_list]
 
     except (Exception, psycopg2.Error) as error:
         error_logger.error("Error while connecting to PostgreSQL %s ", error)
