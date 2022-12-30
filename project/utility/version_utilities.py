098468f978ed7585074c80a476e569ad844fafae
ankit-grg <ankit.garg@likeminds.community>
2022-12-30 12:26:39 +0530

    Updated web version code for create chatroom revamp.

--- project/utility/version_utilities.py
+++ project/utility/version_utilities.py
@@ -22,7 +22,7 @@ class VersionUtilities:
     create_chatroom_revamp: dict = {
         PlatformCode.ANDROID: beta_dummy_version,
         PlatformCode.IOS: beta_dummy_version,
-        PlatformCode.WEB: beta_dummy_version,
+        PlatformCode.WEB: 14,
         PlatformCode.ANDROID_SDK: 207,
         PlatformCode.IOS_SDK: beta_dummy_version,
         PlatformCode.WEB_SDK: 14
