0925ab065519c4b7be105434037f8994339eca9e
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2023-01-03 17:46:18 +0530

    [LM-7780] Update community notification settings script (#4972)
    * Updated script for updating notification settings for all users in community.
    
    * Added ketan's comments.
    
    * Added ketan's comments 2.

--- project/scripts/update_notification_settings_for_user_in_community.py
+++ project/scripts/update_notification_settings_for_user_in_community.py
@@ -6,44 +6,52 @@ from collabmates_api.chatroom.chatroom_impl import (ChatroomImpl)
 from django.conf import settings
 
 if settings.IS_BETA:
-    notification_state = noti_states.ALL_MESSAGES
+    notification_state = noti_states.ONLY_MENTIONS_AND_REPLIES
     chatroom_id = None
-    user_id = 3877
+    user_id = None
+    community_id = None
 
     filter_dict = {
-        'user': user_id,
-        'community': 50429,
-        'follow_status': True,
+        'community': community_id,
         'remove': None
     }
 
 else:
     notification_state = noti_states.ALL_MESSAGES
     chatroom_id = None
-    user_id = 3877
+    community_id = None
+    user_id = None
 
     filter_dict = {
-        'user': user_id,
-        'community': 2,
-        'follow_status': True,
+        'community': community_id,
         'remove': None
     }
 
 
-def update_notification_state_for_user_in_community():
+def update_notification_state_for_users_in_community():
     if chatroom_id:
         filter_dict['card'] = chatroom_id
 
+    if user_id:
+        filter_dict['user'] = user_id
+
     card_state_filter = ModelUtilities.get_model_filter(collabcardState, filter_dict)
 
-    if card_state_filter:
+    count = card_state_filter.count()
 
-        for card_state_instance in card_state_filter:
-            ChatroomImpl(member_id=user_id, chatroom_id=card_state_instance.card_id).update_chatroom_noti_settings(
-                noti_state=notification_state, is_noti_paused=None, pause_noti_for=None)
+    print("Records to be updated ->", count)
+
+    for card_state_instance in card_state_filter:
+        print(count)
+
+        ChatroomImpl(member_id=card_state_instance.user_id,
+                     chatroom_id=card_state_instance.card_id).update_chatroom_noti_settings(
+            noti_state=notification_state, is_noti_paused=None, pause_noti_for=None)
+
+        count -= 1
 
 
 start = time.time()
 print('Starting script!')
-update_notification_state_for_user_in_community()
+update_notification_state_for_users_in_community()
 print('Script completed in ->', time.time() - start)
