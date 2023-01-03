8f2503f43d2ae1eb6c054736c84cb6c2d9100fda
ankit-grg <ankit.garg@likeminds.community>
2023-01-03 16:25:04 +0530

    Added ketan's comments.

--- project/scripts/update_notification_settings_for_user_in_community.py
+++ project/scripts/update_notification_settings_for_user_in_community.py
@@ -8,20 +8,22 @@ from django.conf import settings
 if settings.IS_BETA:
     notification_state = noti_states.ONLY_MENTIONS_AND_REPLIES
     chatroom_id = None
+    user_id = None
+    community_id = None
 
     filter_dict = {
-        'community': 50430,
+        'community': community_id,
         'remove': None
     }
 
 else:
     notification_state = noti_states.ALL_MESSAGES
     chatroom_id = None
+    community_id = 2
+    user_id = None
 
     filter_dict = {
-        'user': 3877,
-        'community': 2,
-        'follow_status': True,
+        'community': community_id,
         'remove': None
     }
 
@@ -29,6 +31,9 @@ else:
 def update_notification_state_for_users_in_community():
     if chatroom_id:
         filter_dict['card'] = chatroom_id
+
+    if user_id:
+        filter_dict['user'] = user_id
 
     card_state_filter = ModelUtilities.get_model_filter(collabcardState, filter_dict)
 
