c95a16f25eb42e73e3e4aed01ee30e5359fe833c
ankit-grg <ankit.garg@likeminds.community>
2023-01-27 17:58:40 +0530

    Added unique constraint in Members table and also added script for removing duplicate records.

--- project/togther/models.py
+++ project/togther/models.py
@@ -211,6 +211,9 @@ class Members(models.Model):
 
     has_onboarded = models.BooleanField(default=False)
 
+    class Meta:
+        unique_together = (('community_id', 'member_id'),)
+
     @staticmethod
     def create_instance(create_info):
         member_instance = Members()
