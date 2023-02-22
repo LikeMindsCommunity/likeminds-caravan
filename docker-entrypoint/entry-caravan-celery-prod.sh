613023caacac6535e6d6e25b9be93b14d6c539af
Ketan Yadav <ketan@likeminds.community>
2023-02-22 12:11:29 +0530

    Add production dockerfiles

---
+++ docker-entrypoint/entry-caravan-celery-prod.sh
@@ -0,0 +1,7 @@
+mkdir /var/log/celery
+touch /var/log/celery/celery.log
+touch /var/log/celery/celery_beat.log
+source /opt/venv/bin/activate
+cd ./project
+DJANGO_SETTINGS_MODULE=project.settings.production celery -A project worker --loglevel=info -f /var/log/celery/celery.log
+DJANGO_SETTINGS_MODULE=project.settings.production celery -A project beat --loglevel=info -f /var/log/celery/celery_beat.log --scheduler django_celery_beat.schedulers:DatabaseScheduler
