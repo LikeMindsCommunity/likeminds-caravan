f27fda9501ee6af60f1d9b49acad3e3d657a3fb2
ankit-grg <ankit.garg@likeminds.community>
2023-02-23 21:55:44 +0530

    Grammar mistakes.

--- project/collabmates_api/notification.py
+++ project/collabmates_api/notification.py
@@ -144,7 +144,7 @@ def send_notification_for_android(token_list, message, firebase_key=None):
 
     # print(result)
     log_statement = """
-        The {} devices should has total {} notifications out of which {} success & {} failures. Payload is {}
+        The {} devices should have total {} notifications out of which {} success & {} failures. Payload is {}
     """.format("ANDROID", len(token_list), result.get('success'), result.get('failure'), message.get('payload'))
     print(log_statement)
     return result
@@ -174,7 +174,7 @@ def send_notification_for_ios(token_list, message, firebase_key=None):
 
     # print(result)
     log_statement = """
-            The {} devices should has total {} notifications out of which {} success & {} failures. Payload is {}
+            The {} devices should have total {} notifications out of which {} success & {} failures. Payload is {}
         """.format("IOS", len(token_list), result.get('success'), result.get('failure'), message.get('payload'))
     print(log_statement)
     return result
