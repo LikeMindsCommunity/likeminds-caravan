92100e929326a94fdc73a48faea1b6461d833177
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2023-02-25 12:17:12 +0530

    [LM-8021] Sync revamp misses (#5035)
    * Added sync revamp misses.
    
    * Added sync revamp misses version 2.

--- project/collabmates_api/sync/constants.py
+++ project/collabmates_api/sync/constants.py
@@ -2,14 +2,23 @@ SYNC_KEY_SPLIT_VALUE = '___'
 IGNORED_KEYS_LIST = ['row_number']
 META_KEYS_SUFFIX = '_meta'
 SYNC_RESPONSE_MAP_PRIMARY_KEYS = ['id', 'user_id_id', 'member_id_id']
+SYNC_CHATROOMS_DATA_KEY = 'chatrooms_data'
+SYNC_CONVERSATIONS_DATA_KEY = 'conversations_data'
 USERS_META_KEY_VALUE = 'user_meta'
 MEMBERS_META_KEY_VALUE = 'member_meta'
 CONVERSATIONS_META_KEY_VALUE = 'conversation_meta'
+CONVERSATION_POLLS_META_KEY_VALUE = 'conv_polls_meta'
 COMMUNITY_META_KEY_VALUE = 'community_meta'
 CHATROOM_META_KEY_VALUE = 'chatroom_meta'
+CHATROOM_STATE_META_KEY_VALUE = 'chatroom_state_meta'
 MESSAGE_REACTIONS_META_KEY_VALUE = 'message_reactions_meta'
 MAIN_PRIMARY_KEY_VALUE = 'id'
+CONVERSATION_STATE_KEY_VALUE = 'state'
+POLL_CONVERSATION_TO_SHOW_RESULTS_KEY = 'to_show_results'
+CONVERSATIONS_CREATED_AT_KEY = 'created_at'
+CONVERSATIONS_DATE_KEY = 'date'
+CONVERSATIONS_CREATED_EPOCH_KEY = 'created_epoch'
 SYNC_DATA_KEYS = {
     'conversation___id___last': 'last_conversation_id'
 }
-PARSE_JSON_KEYS = ['secret_chatroom_participants', 'meta']
+PARSE_JSON_KEYS = ['secret_chatroom_participants', 'meta', 'og_tags']
--- project/collabmates_api/sync/sync_helper.py
+++ project/collabmates_api/sync/sync_helper.py
@@ -2,10 +2,15 @@ from utility.response_utilities import ResponseUtilities
 from utility.validation_utilities import ValidationUtilities
 from utility.json_utilities import JsonUtilities
 from utility.time_utilities import TimeUtilities
+from utility.number_utilities import NumberUtilities
 from .constants import (SYNC_KEY_SPLIT_VALUE, IGNORED_KEYS_LIST, META_KEYS_SUFFIX, SYNC_RESPONSE_MAP_PRIMARY_KEYS,
                         USERS_META_KEY_VALUE, MEMBERS_META_KEY_VALUE, MAIN_PRIMARY_KEY_VALUE,
                         CONVERSATIONS_META_KEY_VALUE, SYNC_DATA_KEYS, COMMUNITY_META_KEY_VALUE,
-                        CHATROOM_META_KEY_VALUE, PARSE_JSON_KEYS, MESSAGE_REACTIONS_META_KEY_VALUE)
+                        CHATROOM_META_KEY_VALUE, PARSE_JSON_KEYS, MESSAGE_REACTIONS_META_KEY_VALUE,
+                        CONVERSATION_STATE_KEY_VALUE, POLL_CONVERSATION_TO_SHOW_RESULTS_KEY,
+                        CONVERSATION_POLLS_META_KEY_VALUE, CONVERSATIONS_DATE_KEY, CHATROOM_STATE_META_KEY_VALUE,
+                        CONVERSATIONS_CREATED_EPOCH_KEY, CONVERSATIONS_CREATED_AT_KEY)
+from utility.states import (conversation_states, conversation_poll_types)
 
 
 class SyncHelper:
@@ -65,8 +70,14 @@ class SyncHelper:
         if not max_timestamp:
             max_timestamp = TimeUtilities.current_time_in_sec()
 
+        else:
+            max_timestamp = NumberUtilities.get_integer_from_string(max_timestamp, TimeUtilities.current_time_in_sec())
+
         if not min_timestamp:
             min_timestamp = 0
+
+        else:
+            min_timestamp = NumberUtilities.get_integer_from_string(min_timestamp, return_default=0)
 
         user_instance = validated_dict.get('user_id')
         community_instance = validated_dict.get('community_id')
@@ -81,7 +92,8 @@ class SyncHelper:
         }
 
     @staticmethod
-    def merge_meta_data(primary_data: dict, secondary_data: dict = None, primary_key: str = MAIN_PRIMARY_KEY_VALUE):
+    def merge_meta_data(primary_data: dict, secondary_data: dict = None, primary_key: str = MAIN_PRIMARY_KEY_VALUE,
+                        secondary_key: str = None):
         merged_meta_data = {}
 
         if not primary_data:
@@ -103,27 +115,30 @@ class SyncHelper:
             return merged_meta_data
 
         primary_data = secondary_data
+        secondary_key = primary_key if not secondary_key else secondary_key
 
         if isinstance(list(primary_data.values())[0], dict):
 
             for key, data in primary_data.items():
 
-                if data.get(primary_key):
-                    merged_meta_data[data.get(primary_key)].update(data)
+                if data.get(secondary_key):
+                    merged_meta_data[data.get(secondary_key)].update(data)
 
         else:
 
-            if primary_data.get(primary_key):
-                merged_meta_data[primary_data.get(primary_key)].update(primary_data)
+            if primary_data.get(secondary_key):
+                merged_meta_data[primary_data.get(secondary_key)].update(primary_data)
 
         return merged_meta_data
 
     @staticmethod
     def combine_and_convert_dicts_to_sync_meta_data(data, resulting_dict, primary_key, secondary_key: str = None,
-                                                    resulting_primary_key: str = MAIN_PRIMARY_KEY_VALUE):
+                                                    resulting_primary_key: str = MAIN_PRIMARY_KEY_VALUE,
+                                                    secondary_data_merging_key: str = None):
         filter_dict = {
             'primary_data': data.get(primary_key, {}),
-            'primary_key': resulting_primary_key
+            'primary_key': resulting_primary_key,
+            'secondary_key': secondary_data_merging_key
         }
 
         if secondary_key:
@@ -236,6 +251,10 @@ class SyncHelper:
             sync_response = SyncHelper.combine_and_convert_dicts_to_sync_meta_data(
                 parsed_meta_data, sync_response, primary_key=MESSAGE_REACTIONS_META_KEY_VALUE)
 
+            sync_response = SyncHelper.combine_and_convert_dicts_to_sync_meta_data(
+                parsed_meta_data, sync_response, primary_key=CHATROOM_META_KEY_VALUE,
+                secondary_key=CHATROOM_STATE_META_KEY_VALUE, secondary_data_merging_key='card_id')
+
         sync_response[sync_data_key] = parsed_data
 
         return sync_response
@@ -265,3 +284,75 @@ class SyncHelper:
                                                                            primary_key=USERS_META_KEY_VALUE)
 
         return sync_data
+
+    @staticmethod
+    def compute_show_poll_results_for_conversation_meta(conv_data: dict, user_id: int, is_user_cm: bool = None,
+                                                        conv_polls_data: list = None):
+        to_show_results = False
+
+        if conv_data.get(CONVERSATION_STATE_KEY_VALUE) != conversation_states.CONVERSATION_POLL:
+            return to_show_results
+
+        elif is_user_cm:
+            to_show_results = True
+
+        elif conv_data.get('user_id') == user_id:
+            to_show_results = True
+
+        elif not conv_data.get('expiry_time'):
+            to_show_results = True
+
+        elif conv_data.get('expiry_time') <= TimeUtilities.current_time_in_milliseconds():
+            to_show_results = True
+
+        elif conv_data.get('poll_type') == conversation_poll_types.INSTANT and conv_polls_data:
+
+            for poll_selection_data in conv_polls_data:
+
+                if poll_selection_data.get('is_selected'):
+                    return True
+
+        return to_show_results
+
+    @staticmethod
+    def compute_conversation_additional_data(conversation_data, user_id: int, is_user_cm: bool = False,
+                                             conv_polls_data: list = None):
+
+        if not conv_polls_data:
+            conv_polls_data = {}
+
+        conversation_data[POLL_CONVERSATION_TO_SHOW_RESULTS_KEY] = \
+            SyncHelper.compute_show_poll_results_for_conversation_meta(
+                conversation_data, user_id, is_user_cm, conv_polls_data)
+
+        if conversation_data.get(CONVERSATIONS_CREATED_AT_KEY):
+            conversation_data[CONVERSATIONS_CREATED_EPOCH_KEY] = conversation_data.get(CONVERSATIONS_CREATED_AT_KEY)
+
+            conversation_data[CONVERSATIONS_DATE_KEY] = TimeUtilities.convert_epoch_time_in_date(
+                conversation_data.get(CONVERSATIONS_CREATED_AT_KEY))
+
+            conversation_data[CONVERSATIONS_CREATED_AT_KEY] = TimeUtilities.convert_epoch_time_in_hh_mm(
+                conversation_data.get(CONVERSATIONS_CREATED_AT_KEY))
+
+    @staticmethod
+    def add_additional_data_in_conversation_meta(sync_data,
+                                                 user_id: int,
+                                                 conversation_data_key: str = CONVERSATIONS_META_KEY_VALUE,
+                                                 is_user_cm: bool = False):
+        conversations_data = sync_data.get(conversation_data_key)
+        conv_polls_data = sync_data.get(CONVERSATION_POLLS_META_KEY_VALUE)
+
+        if isinstance(conversations_data, dict):
+            conversations_data_list = list(conversations_data.values())
+
+        elif isinstance(conversations_data, list):
+            conversations_data_list = conversations_data
+
+        else:
+            return
+
+        for conversation_data in conversations_data_list:
+            SyncHelper.compute_conversation_additional_data(conversation_data,
+                                                            user_id,
+                                                            is_user_cm,
+                                                            conv_polls_data.get(conversation_data.get('id')))
