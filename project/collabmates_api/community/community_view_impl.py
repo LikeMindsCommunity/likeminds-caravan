c30a02b58b265bc52181cafae6f38b6863cbdefa
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-02-27 10:35:08 +0530

    Using get_platform_code instead of sdk

--- project/collabmates_api/community/community_view_impl.py
+++ project/collabmates_api/community/community_view_impl.py
@@ -329,7 +329,8 @@ class FetchMembersMeta(APIView):
     def get(self, request, *args, **kwargs):
         member_id = RequestUtilities.get_member_id_from_headers(request)
         api_key = RequestUtilities.get_api_key_from_headers(request)
-        platform_code = RequestUtilities.get_platform_code_with_sdk(request)
+        # Using get_platform_code instead of get_platform_code_with_sdk for FLUTTER support
+        platform_code = RequestUtilities.get_platform_code(request)
         version_code = RequestUtilities.get_version_code_from_headers(request)
         
         community_id = request.GET.get('community_id')
