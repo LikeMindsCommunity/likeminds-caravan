6a2ab0b3d0b217f477554d02bef7145722269ba4
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2023-03-03 12:10:48 +0530

    Added support for notification settings in unread_conversation_notification API. (#5048)

--- project/collabmates_api/notification.py
+++ project/collabmates_api/notification.py
@@ -1227,24 +1227,32 @@ def compute_mute_status_for_users(current_user_id):
     return mute_list
 
 
-def get_custom_data_for_new_conversation_created(user_id: str, community_id: str) -> list:
+def get_custom_data_for_new_conversation_created(user_id: str, community_id: str, page_size: int = 10) -> list:
     """function to send notification for new conversation posted to followed users"""
 
     mute_status_list = compute_mute_status_for_users(user_id)
 
     filter_dict = _get_conversation_engage_filter_for_new_conversation(user_id, community_id)
 
-    followed_chatrooms = conversationEngage.objects.filter(
-        **filter_dict
-    ).filter(
-        ~Q(card_id__in=mute_status_list)
+    followed_chatrooms = ModelUtilities.get_model_filter(conversationEngage, filter_dict)
+
+    followed_chatrooms_ids_list = list(followed_chatrooms.values_list('card_id', flat=True))
+
+    from collabmates_api.raw_queries import get_excluded_chatroom_ids_for_notification_settings_for_user
+    excluded_card_ids = get_excluded_chatroom_ids_for_notification_settings_for_user(
+        user_id, chatroom_ids_list=followed_chatrooms_ids_list)
+
+    excluded_card_ids = list(set(mute_status_list + excluded_card_ids))
+
+    followed_chatrooms = followed_chatrooms.exclude(
+        card_id__in=excluded_card_ids
     ).select_related(
         'card',
         'community'
     ).order_by(
         '-updated_at',
         '-id'
-    )[:10]
+    )[:page_size]
 
     unread_conversation = []
 
