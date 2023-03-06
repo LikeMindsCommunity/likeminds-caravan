75222505356e7051aef086a5d6e08203b540f441
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2023-03-06 15:15:12 +0530

    [LM-8147] Changing route in notification Payload for GroupTags in conversation/create (#5057)
    * Changing route for GroupTag in conversation/create
    * Grammer changes

--- project/collabmates_api/static_text.py
+++ project/collabmates_api/static_text.py
@@ -505,6 +505,7 @@ CHATROOM_TOPIC_NOTIFICATION_SUB_TITLE = "The topic of your followed chat room %s
 CHATROOM_TOPIC_NOTIFICATION_ROUTE = "route://chatroom_detail?chatroom_id=%s"
 
 CHATROOM_DETAIL_NOTIFICATION_ROUTE = "route://chatroom_detail?chatroom_id=%s"
+COLLABCARD_COMMUNITY_NOTIFICATION_ROUTE = "route://collabcard?collabcard_id=%s&community_id=%s"
 SECRET_CHATROOM_VERSION_CODE_IOS = 157
 
 PAID_COMMUNITY_LEVEL_4_TITLE = "Monetization"
