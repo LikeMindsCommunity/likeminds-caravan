3011e79f2297ec7a778321773dc8391cb3ead2b3
Ketan Yadav <ketan@likeminds.community>
2023-03-23 13:25:57 +0530

    Update gunicorn workers for caravan-prod docker entrypoint

--- docker-entrypoint/entry-caravan-prod.sh
+++ docker-entrypoint/entry-caravan-prod.sh
@@ -1,5 +1,3 @@
 source /opt/venv/bin/activate
 cd ./project
-DJANGO_SETTINGS_MODULE=project.settings.production python3 manage.py makemigrations
-DJANGO_SETTINGS_MODULE=project.settings.production python3 manage.py migrate
-DJANGO_SETTINGS_MODULE=project.settings.production gunicorn --bind 0.0.0.0:8081 project.wsgi
+DJANGO_SETTINGS_MODULE=project.settings.production gunicorn --workers=9 --bind 0.0.0.0:8081 project.wsgi
