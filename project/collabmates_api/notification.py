197bb1b825aad3b37e7da55085fe7819b87235b3
ankit-grg <ankit.garg@likeminds.community>
2023-03-28 17:11:40 +0530

    Changed notification payload for react native.

--- project/collabmates_api/notification.py
+++ project/collabmates_api/notification.py
@@ -262,7 +262,7 @@ def send_notification_for_react_native(token_list, message, firebase_key=None):
 
     push_service = FCMNotification(api_key=firebase_key)
 
-    extra_notification_kwargs = {
+    extra_kwargs = {
         "android": {
             "priority": "high",
         },
@@ -272,11 +272,9 @@ def send_notification_for_react_native(token_list, message, firebase_key=None):
     }
 
     result = push_service.notify_multiple_devices(registration_ids=token_list,
-                                                  message_title=message['payload']['title'],
-                                                  message_body=message['payload']['sub_title'],
                                                   data_message=message['payload'],
                                                   timeout=fcm_timeout_seconds,
-                                                  extra_notification_kwargs=extra_notification_kwargs)
+                                                  extra_kwargs=extra_kwargs)
 
     log_statement = """
                 The {} devices should have total {} notifications out of which {} success & {} failures. Payload is {}
