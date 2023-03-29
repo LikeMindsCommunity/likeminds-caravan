7a33140ffb4c15fafcecbfd20abb49ed9638db8c
Mahir-G2 <mahir.gupta@likeminds.community>
2023-03-29 18:18:05 +0530

    [LM-8332] Notification Payload Update

--- project/collabmates_api/notification.py
+++ project/collabmates_api/notification.py
@@ -242,7 +242,7 @@ def send_notification_for_flutter(token_list, message, firebase_key=None):
                                                   message_body=message['payload']['sub_title'],
                                                   data_message=message['payload'],
                                                   timeout=fcm_timeout_seconds,
-                                                  extra_notification_kwargs=extra_notification_kwargs)
+                                                  extra_kwargs=extra_notification_kwargs)
 
     log_statement = """
                 The {} devices should have total {} notifications out of which {} success & {} failures. Payload is {}
@@ -272,6 +272,8 @@ def send_notification_for_react_native(token_list, message, firebase_key=None):
     }
 
     result = push_service.notify_multiple_devices(registration_ids=token_list,
+                                                  message_title=message['payload']['title'],
+                                                  message_body=message['payload']['sub_title'],
                                                   data_message=message['payload'],
                                                   timeout=fcm_timeout_seconds,
                                                   extra_kwargs=extra_kwargs)
