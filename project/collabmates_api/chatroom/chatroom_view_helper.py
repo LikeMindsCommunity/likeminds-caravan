72d2c7729d93a13125efaeea24daefa221780446
ankit-grg <ankit.garg@likeminds.community>
2023-04-20 13:46:59 +0530

    Added support of clients user unique id in create_dm API.

--- project/collabmates_api/chatroom/chatroom_view_helper.py
+++ project/collabmates_api/chatroom/chatroom_view_helper.py
@@ -374,7 +374,8 @@ class ChatroomViewHelper:
         if not community_instance:
             return ResponseUtilities.get_inner_error_context("Invalid community id")
 
-        member_instance = ModelUtilities.get_user_instance_or_none(req_body.get('member_id'))
+        member_instance = ModelUtilities.get_user_instance_or_none(req_body.get('member_id'),
+                                                                   community_id=community_instance.id)
 
         if not member_instance:
             return ResponseUtilities.get_inner_error_context("Invalid member id")
