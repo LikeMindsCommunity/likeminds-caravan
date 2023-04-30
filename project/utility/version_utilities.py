9c8aefdee5fc832af5a62d148ce8149c2f8a6f2c
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-04-30 15:23:57 +0530

    flutterr sdk support

--- project/utility/version_utilities.py
+++ project/utility/version_utilities.py
@@ -91,7 +91,7 @@ class VersionUtilities:
             PlatformCode.WEB: 16,
 
             PlatformCode.ANDROID_SDK: 207,
-            PlatformCode.FLUTTER_SDK: unreleased_version_code,
+            PlatformCode.FLUTTER_SDK: 1,
             PlatformCode.IOS_SDK: 372,
             PlatformCode.REACT_SDK: 21,
             PlatformCode.REACT_NATIVE_SDK: 4,
