1737e7b4a01472c3a431778a7ac1508a966bf093
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-05-04 15:57:27 +0530

    added community_id in UserChannelSettings

--- project/togther/models.py
+++ project/togther/models.py
@@ -3560,6 +3560,7 @@ class ChatroomInvite(models.Model):
 
 class UserChannelSettings(models.Model):
     chatroom = models.ForeignKey(Collabcard, on_delete=models.CASCADE)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
     member = models.ForeignKey(User, on_delete=models.CASCADE)
     setting_type = models.CharField(max_length=255)
     enabled = models.BooleanField(default=True)
