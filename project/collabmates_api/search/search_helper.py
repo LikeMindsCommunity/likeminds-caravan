4b65da333bbc9bfe57360a33b89985d154b73292
ankit-grg <ankit.garg@likeminds.community>
2023-05-10 15:06:37 +0530

    Added a minor change.

--- project/collabmates_api/search/search_helper.py
+++ project/collabmates_api/search/search_helper.py
@@ -1,6 +1,7 @@
 from togther.models import (ModelUtilities, Collabcard, Members)
 from collabmates_api.serializers import (get_menu_for_members)
 from utility.states import (member_states)
+from collabmates_api.user_moderation_rights import (check_all_manager_rights)
 
 
 class SearchHelper:
@@ -52,6 +53,11 @@ class SearchHelper:
 
         current_user_is_promoter = current_member_instance.state == member_states.ADMIN
 
+        user_admin_rights = None
+
+        if current_member_instance.is_owner or current_user_is_promoter:
+            user_admin_rights = check_all_manager_rights(current_user_id, community_id)
+
         return get_menu_for_members(current_user_id=current_user_id,
                                     item_member_id=user_id,
                                     community_id=community_id,
@@ -59,4 +65,5 @@ class SearchHelper:
                                     current_user_is_owner=current_member_instance.is_owner,
                                     item_member_state=user_data.get('state'),
                                     item_member_is_owner=user_data.get('is_owner'),
-                                    parents_list=user_data.get('parent_cm_list'))
+                                    parents_list=user_data.get('parent_cm_list'),
+                                    current_user_admin_rights=user_admin_rights)
