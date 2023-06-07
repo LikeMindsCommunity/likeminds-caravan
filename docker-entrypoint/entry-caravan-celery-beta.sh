082b275b17fbf118477142ddc84df8e07fab3bbf
Mahir-G2 <mahir.gupta@likeminds.community>
2023-06-08 00:35:10 +0530

    scheduler settings changes

--- docker-entrypoint/entry-caravan-celery-beta.sh
+++ docker-entrypoint/entry-caravan-celery-beta.sh
@@ -3,4 +3,4 @@ touch /var/log/celery/celery.log
 touch /var/log/celery/celery_beat.log
 source /opt/venv/bin/activate
 cd ./project
-DJANGO_SETTINGS_MODULE=project.settings.beta celery -A project worker --beat --loglevel=info -f /var/log/celery/celery.log
+DJANGO_SETTINGS_MODULE=project.settings.beta celery -A project worker --beat --loglevel=info -f /var/log/celery/celery.log --scheduler django_celery_beat.schedulers:DatabaseScheduler
