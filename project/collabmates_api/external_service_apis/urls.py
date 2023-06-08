95ec75e2cb44ec18adbf4461b3e5f09928627a3a
Mahir-G2 <76210826+Mahir-G2@users.noreply.github.com>
2023-06-09 01:25:39 +0530

    [LM - 8789] Usage Feature Changes (#5243)
    * [LM-8789] Usage Feature
    
    * changes after local testing
    
    * [LM-8789] Usage Feature cron job changes
    
    * changes for peer testing on beta
    
    * [LM-8789] Updated coralogix settings
    
    * [LM-8789] Celery Related changes
    
    * [LM-8789] Usage Feature Celery Changes
    
    * [LM-8789] Usage Feature Testing Changes
    
    * [LM-8789] Celery beat testing changes
    
    * added EOF line
    
    * logger removed from cron job
    
    * scheduler settings changes
    
    * temporary coralogix removal
    
    * removed comment text
    
    * updated log level
    
    * added coralogix again
    
    * updated after testing
    
    * celery changes removed
    
    * reverted celery changes
    
    * [LM-8789] Exposed API Endpoint for cron jobs
    
    * updated response

--- project/collabmates_api/external_service_apis/urls.py
+++ project/collabmates_api/external_service_apis/urls.py
@@ -4,5 +4,6 @@ from .external_service_apis_view_impl import *
 urlpatterns = [
     path('send_email', SendEmailView.as_view(), name="send_email"),
     path('send_wa_bulk_messages', SendWhatsAppMessageView.as_view(), name="send_wa_bulk_message"),
-    path('send_notifications', SendNotificationsView.as_view(), name="send_notifications")
+    path('send_notifications', SendNotificationsView.as_view(), name="send_notifications"),
+    path('crontab/<str:task_name>', RunCronJobView.as_view(), name="run_cron_jobs")
 ]
