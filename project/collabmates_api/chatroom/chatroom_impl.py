dda70daea0704f924dfe306106f02baafd0cac6b
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-06-08 15:57:21 +0530

    Added CUUID in fetch_members_meta old flow

--- project/collabmates_api/chatroom/chatroom_impl.py
+++ project/collabmates_api/chatroom/chatroom_impl.py
@@ -586,7 +586,7 @@ class ChatroomImpl(ChatroomManager):
     def compute_tagging_list_of_community_members(community_instance, member_ids=[], search_name: str = None, page: int = None, page_size: int = None, order_by_name: bool = None): 
         member_list = MemberCommunityImpl.fetch_list_of_community_members(community_instance, member_ids)
         member_data = MemberCommunityImpl.fetch_members_based_on_user_list(member_list, community_instance, member_name_search_string = search_name, page = page, page_size=page_size, order_by_name = order_by_name)
-        tagging_list = MemberCommunityHelper.extract_member_tagging_data(member_data)
+        tagging_list = MemberCommunityHelper.extract_member_tagging_data(member_data, sdk_client_info_flag=True)
 
         return tagging_list
 
