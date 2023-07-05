563f4923f705a194fcf5c94cdaf22753820ac0a1
Ketan Yadav <ketan@likeminds.community>
2023-07-06 00:04:11 +0530

    Add stream handler to coralogix logger

--- project/external_services/logging/coralogix_logger.py
+++ project/external_services/logging/coralogix_logger.py
@@ -19,6 +19,7 @@ class CoralogixLoggerImpl(LoggerManager):
         handler = self._coralogix_handler()
         handler.setLevel(logging.INFO)
         logger.addHandler(handler)
+        logger.addHandler(logging.getLogger('stream_info_logger'))
 
         return logger
 
