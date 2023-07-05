5b140f7e68faf60b15af3d3316da2d89e5f6c19d
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-07-05 12:38:49 +0530

    Changed deleted_by_member to deleted_by_user

--- project/collabmates_api/rest_api.py
+++ project/collabmates_api/rest_api.py
@@ -1188,7 +1188,7 @@ class CardAnswersDBSyncSerializer(serializers.ModelSerializer):
                 del data[field.field_name]
 
             elif field.field_name == 'deleted_by' and data['deleted_by']:
-                data['deleted_by_user'] = self.get_serialised_userinfo(data['deleted_by'])
+                data['deleted_by_member'] = self.get_serialised_userinfo(data['deleted_by'])
 
         return data
 
