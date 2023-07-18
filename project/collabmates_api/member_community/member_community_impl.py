16ac8208d8ecc6e3545ffbc5cb7b4808a244eeb0
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2023-07-18 15:17:51 +0530

    [LM-9206] Community/feed removed logs (#5337)

--- project/collabmates_api/member_community/member_community_impl.py
+++ project/collabmates_api/member_community/member_community_impl.py
@@ -85,22 +85,6 @@ from collabmates_api.search.sync import ElasticSearchSync
 error_logger = LoggingWrapper.get_instance()
 
 
-def timeit(func):
-    from functools import wraps
-    import time
-
-    @wraps(func)
-    def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        error_logger.error(f'COMMUNITY/FEED Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
-        return result
-
-    return timeit_wrapper
-
-
 class MemberCommunityImpl(MemberCommunityManager):
     member_id = None
     community_id = None
@@ -873,7 +857,6 @@ class MemberCommunityImpl(MemberCommunityManager):
                                                                          ).order_by('created_at')
         return member_queryset
 
-    @timeit
     def fetch_feed(self, pin_status, order_type, chatroom_id=None, scroll_direction=None, api_version="", page=1, 
                    community_feed_date_uniform_check=False) -> {}:
 
@@ -894,8 +877,6 @@ class MemberCommunityImpl(MemberCommunityManager):
             'enabled': True
         }
 
-        error_logger.error(f"COMMUNITY/FEED Before version check 1 {self.get_member_id()}")
-
         if is_version_code_supported_for_intro_room(self.get_version_code(), self.get_platform_code()):
             intro_room_setting_enabled = False
 
@@ -907,11 +888,7 @@ class MemberCommunityImpl(MemberCommunityManager):
         else:
             intro_room_setting_enabled = True
 
-        error_logger.error(f"COMMUNITY/FEED After version check 1 {self.get_member_id()}")
-
         excluded_card_ids = []
-
-        error_logger.error(f"COMMUNITY/FEED Start computing cohort access cards {self.get_member_id()}")
 
         if create_chatroom_revamp_version_check(self.get_platform_code(), self.get_version_code()):
             excluded_card_ids = get_card_ids_to_exclude_based_on_cohort_access(self.get_member_id(),
@@ -921,16 +898,12 @@ class MemberCommunityImpl(MemberCommunityManager):
 
             excluded_card_ids = list(set(excluded_card_ids) - set(followed_card_ids))
 
-        error_logger.error(f"COMMUNITY/FEED Computed cohort access cards {self.get_member_id()}")
-
         if api_version in [api_version_headers.V1, api_version_headers.V2]:
-            error_logger.error(f"COMMUNITY/FEED Getting chatroom instances {self.get_member_id()}")
             chatroom_list = self._get_sorted_chatroom_queryset_based_on_order_type(intro_room_setting_enabled,
                                                                                    pin_status, excluded_card_ids,
                                                                                    order_type, page=page,
                                                                                    api_version=api_version)
-            error_logger.error(f"COMMUNITY/FEED Computed chatroom instances {self.get_member_id()}")
-
+            
         else:
 
             if not chatroom_id and not scroll_direction:
@@ -965,8 +938,6 @@ class MemberCommunityImpl(MemberCommunityManager):
 
         chatroom_member_impl = ChatroomMemberImpl(member_id=self.get_member_id(), device_id=self.device_id)
 
-        error_logger.error(f"COMMUNITY/FEED Serializing chatrooms {self.get_member_id()}")
-
         chatroom_context_list = chatroom_member_impl.process_chatroom_list(chatroom_list, community_instance,
                                                                            sdk_client_info_flag=True)
 
@@ -975,12 +946,8 @@ class MemberCommunityImpl(MemberCommunityManager):
             for chatroom in chatroom_context_list:
                 chatroom['created_at'] = chatroom['date_epoch']
 
-        error_logger.error(f"COMMUNITY/FEED Serialized chatrooms {self.get_member_id()}")
-
         pinned_chatrooms_list = MemberCommunityHelper.get_pinned_chatrooms_in_community_from_cache(
             community_id=community_instance.id)
-
-        error_logger.error(f"COMMUNITY/FEED Computing pinned chatrooms list {self.get_member_id()}")
 
         return {
             'success': True,
@@ -1650,7 +1617,6 @@ class MemberCommunityImpl(MemberCommunityManager):
 
         return {'success': True}
 
-    @timeit
     def _get_sorted_chatroom_queryset_based_on_order_type(self, intro_room_settings_enabled, pin_status,
                                                           excluded_card_ids, order_type, page=1,
                                                           api_version=api_version_headers.V1, limit=10):
