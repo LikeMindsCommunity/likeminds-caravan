7e7576b28413f831e09f96765127407a13d636ec
ankit-grg <ankit.garg@likeminds.community>
2023-08-24 17:05:21 +0530

    Added community hood check for participants meta pagination.

--- project/collabmates_api/chatroom/chatroom_impl.py
+++ project/collabmates_api/chatroom/chatroom_impl.py
@@ -1878,9 +1878,14 @@ class ChatroomImpl(ChatroomManager):
                                                                       VersionUtilities.participants_meta_pagination,
                                                                       self.get_sdk_source())
 
+            community_hood_version_check = VersionUtilities.check_version(self.get_request_platform(),
+                                                                          self.get_version_code(),
+                                                                          VersionUtilities.community_hood,
+                                                                          self.get_sdk_source())
+
             order_by_name = False
 
-            if pagination_version_check:
+            if pagination_version_check and (not community_hood_version_check):
                 order_by_name = True
 
             participant_list = self.compute_tagging_list_for_secret_participants(
@@ -1895,7 +1900,7 @@ class ChatroomImpl(ChatroomManager):
                 'can_edit_participant': can_edit_participant
             }
 
-            if pagination_version_check:
+            if pagination_version_check and (not community_hood_version_check):
                 participants_count = ChatroomHelper.get_participants_count_in_chatroom(card_instance)
                 response_dict['total_participants_count'] = participants_count
 
@@ -3078,9 +3083,14 @@ class ChatroomImpl(ChatroomManager):
                                                                   VersionUtilities.participants_meta_pagination,
                                                                   self.get_sdk_source())
 
+        community_hood_version_check = VersionUtilities.check_version(self.get_request_platform(),
+                                                                      self.get_version_code(),
+                                                                      VersionUtilities.community_hood,
+                                                                      self.get_sdk_source())
+
         order_by_name = False
 
-        if pagination_version_check:
+        if pagination_version_check and (not community_hood_version_check):
             order_by_name = True
 
         member_data = MemberCommunityImpl.fetch_members_based_on_user_list(total_participants_list, community_instance,
@@ -3096,7 +3106,7 @@ class ChatroomImpl(ChatroomManager):
             'can_edit_participant': can_edit_participant
         }
 
-        if pagination_version_check:
+        if pagination_version_check and (not community_hood_version_check):
             participants_count = ChatroomHelper.get_participants_count_in_chatroom(card_instance)
             response_dict['total_participants_count'] = participants_count
 
