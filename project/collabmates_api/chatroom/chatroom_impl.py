3809dcdd1e440d10e21e3c2a7662203f1232228d
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2023-09-05 17:53:38 +0530

    [LM-9596] Script for secret chatroom collabcardState bugfix (#5433)
    * script for secret chatroom participants bugfix
    * Added is_secret for ChatroomSecretTypeConversion
    * Added 1 hour check for chatroom type converstion

--- project/collabmates_api/chatroom/chatroom_impl.py
+++ project/collabmates_api/chatroom/chatroom_impl.py
@@ -3575,6 +3575,10 @@ class ChatroomImpl(ChatroomManager):
         if is_secret == card_instance.is_secret:
             return {'success': True}
 
+        if (card_instance.created_at + TimeUtilities.MILLI_SEC_IN_AN_HOUR) > TimeUtilities.current_time_in_milliseconds():
+            return ResponseUtilities.get_impl_error_context('Action not allowed, try again after 1 hour',
+                                                            status_code=status_codes.HTTP_400_BAD_REQUEST)
+
         conversion_filter = ModelUtilities.get_model_filter(ChatroomSecretTypeConversion, {'chatroom': card_instance})
 
         if conversion_filter:
