183b68f180f0041463f10a23a1e086d89bcd0d3a
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-09-16 16:18:04 +0530

    Email changes

--- project/collabmates_api/webhook/constants.py
+++ project/collabmates_api/webhook/constants.py
@@ -7,21 +7,19 @@ WEBHOOK_SOURCE_FEED = "LM_FEED"
 
 WEBHOOK_FAILURE_MAIL_SUBJECT = "Notification for webhook failure"
 WEBHOOK_FAILURE_MAIL_BODY = """
-<h2>Hey Team</h2>
+
+Hey Team,<br>
+{} webhook has failed on {}. <br>
+Please inform the customer about the same.<br>
+
+<h2>Here are the details:</h2>
 <br>
-<h2>
-Webhook Event type: '{}' for URL: <i>{}</i> has failed more than the maximum retry limit.
-<br>
-Hence the webhook has been set as Inactive.
-<br>
-Please check logs for more info.
-</h2>
-<br>
-<h2>Webhook details:</h2> 
-<br>
+Webhook URL: {}<br>
+Webhook Failure Time: {}<br>
+Webhook Status Code: {}<br>
+Webhook Response: {}<br>
+Webhook Payload:<br>
 <code>
 {}
-</code>
-<br>
-<h3>Please inform the concerned team.<h3>
+</code>\
 """
