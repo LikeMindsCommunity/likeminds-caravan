ee75b6dd24085625663bc648afa6c9a330b51436
ankit-grg <ankit.garg@likeminds.community>
2023-10-19 13:06:53 +0530

    Resolved the bug of collabcard states not getting created for new CM.

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -54,7 +54,8 @@ from .utility import *
 from .tasks import (send_verification_mail_for_email_sync, update_pending_chatrooms_and_report_count,
                     update_pending_chatroom_count_for_promoters, update_report_count_for_all_promoters,
                     cm_onboarding_version_check, directory_questions_v2_version_check,
-                    get_user_email_preferred_verified, international_otp_generate_requests_blocked_mail,)
+                    get_user_email_preferred_verified, international_otp_generate_requests_blocked_mail,
+                    create_secret_chatroom_states_for_new_cm, remove_secret_chatroom_states_for_removed_cm)
 from .static_text import ALL_MEMBER_COHORT_TEXT, tool_edit_directory_questions, tool_edit_community_details, \
     tool_community_settings
 from .owner_message_template import post_owner_message_template_in_intro_room, check_owner_template_posted
@@ -11366,7 +11367,10 @@ def update_community_manager_rights(request):
                 member_becomes_cm_dm_chatroom.delay(user_id, community_id, is_m2cm_v2=is_m2cm_v2)
 
                 # Delete user channel settings with settings type as member_can_message for all the members of community
-                delete_user_channel_settings_for_a_user.delay(user_instance.id, community_instance.id, CHATROOM_USER_SETTINGS_MEMBER_CAN_MESSAGE)
+                delete_user_channel_settings_for_a_user.delay(user_instance.id, community_instance.id,
+                                                              CHATROOM_USER_SETTINGS_MEMBER_CAN_MESSAGE)
+
+                create_secret_chatroom_states_for_new_cm.delay(user_instance.id, community_instance.id)
 
             elif custom_title_changed:
                 member_title_changed = True
@@ -11520,6 +11524,7 @@ def remove_community_manager(request):
         if is_user_cm:
             is_m2cm_v2 = m2cm_v2_version_check(platform_code, version_code)
             cm_removed_dm_chatroom.delay(user_id, community_id, is_m2cm_v2=is_m2cm_v2)
+            remove_secret_chatroom_states_for_removed_cm.delay(user_id, community_id)
 
         # Update Members Index
         ElasticSearchSync.update_member.delay(user_id, community_id)
