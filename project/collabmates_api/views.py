b389b56e17ce2e2d1d6ec2ffcf5ebeb9c333d948
ankit-grg <ankit.garg@likeminds.community>
2023-12-08 12:33:11 +0530

    Removed posting of chatroom deletion message in chatroom.

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -3715,6 +3715,7 @@ def chatroom_delete(request):
 
     return JsonResponse(context)
 
+
 @shared_task
 def delete_chatroom_async(member_id, chatroom_id=None, draft_id=None,
                           tag_id=None, reason=None, disallow_create_chatroom=None):
@@ -3805,10 +3806,6 @@ def delete_chatroom_async(member_id, chatroom_id=None, draft_id=None,
 
         # update elastic search
         ElasticSearchSync.delete_chatroom.delay(chatroom_id)
-
-        # Post a delete chatroom message in the chatroom
-        post_state_message_in_chatroom.delay(member_id, chatroom_id, CHATROOM_DELETE_DEFAULT_STATE_MESSAGE,
-                                             conversation_states.CHATROOM_DELETE)
 
     except Exception as e:
         context = get_error_context(False, str(e))
