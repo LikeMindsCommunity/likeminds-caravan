d2b0ef2fd39444ec041452a8785613c376a387a3
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-02-15 21:43:47 +0530

    Delete webhooks cache flow added in Webhooks APIs

--- project/utility/cache_keys.py
+++ project/utility/cache_keys.py
@@ -24,3 +24,4 @@ CHATROOM_TYPE_CONVERSION = "chatroom_type_conversion_{}"
 SYNC_LJ_MIN_TIMESTAMP = "sync_data_{}_{}"
 
 SWARM_CACHE_KEY_CONFIGURATIONS = "%s_community_configurations"
+SWARM_CACHE_KEY_WEBHOOKS = "%s_webhooks"
