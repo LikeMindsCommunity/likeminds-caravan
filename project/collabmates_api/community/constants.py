c9c9940d2f24ba370ad828b1ab665328c6bd7fa8
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2024-03-13 18:10:48 +0530

    [LM-10863] User Topics Connection  (#5615)
    * Added user topics connection settings and Delete kettle cache helper function
    
    * CommunityId change
    
    * Refactor
    
    * Added flow for userMeta changes
    
    * Added flow for community questions changes
    
    * PR changes

--- project/collabmates_api/community/constants.py
+++ project/collabmates_api/community/constants.py
@@ -84,7 +84,8 @@ COMMUNITY_SETTING_TYPE_TITLE_MAPPING = {
     "create_poll": "Create Poll",
     "user_connection": "User Connection",
     "feed_repost": "Feed Repost",
-    "nsfw_filtering": "NSFW Filtering for feed"
+    "nsfw_filtering": "NSFW Filtering for feed",
+    "user_topics_connection": "User Topics Connection"
 }
 
 COMMUNITY_SETTING_TYPE_SUB_TITLE_MAPPING = {
@@ -106,7 +107,8 @@ COMMUNITY_SETTING_TYPE_SUB_TITLE_MAPPING = {
     "create_poll": "",
     "user_connection": "If enabled, the users will be able to send connection request to other members",
     "feed_repost": "If enabled, members can repost other other member posts",
-    "nsfw_filtering": "To enable NSFW filtering for feed posts in the community"
+    "nsfw_filtering": "To enable NSFW filtering for feed posts in the community",
+    "user_topics_connection": "Enable User topics connection (follow/unfollow topics) in the community"
 }
 
 DM_COMMUNITY_SETTING_SUB_TITLE_WHEN_ENABLED = "Community managers will be able to message all members and vice-versa."
@@ -221,10 +223,6 @@ COMMUNITY_REMOVE_MEMBER_S3_BUCKET = "media_bucket"
 COMMUNITY_REMOVE_MEMBER_S3_FILE_PATH = "member_removal_reports/{}/{}_removal_report_{}.csv"
 COMMUNITY_MEMBER_REMOVAL_REPORTS_PATH = "member_removal_reports/{}/"
 COMMUNITYY_MEMBER_REMOVAL_S3_DOWNLOAD_URL = "https://{}.s3.{}.amazonaws.com/{}"
-
-SWARM_USER_FEED_DATA_REMOVAL_ENDPOINT = "/user"
-SWARM_DELETE_CACHE_ENDPOINT = "/cache"
-SWARM_PENDING_POST_UPDATE_ENDPOINT = "/post/pending/{}"
 
 # Report Types string consants
 REPORT_TYPE_MEMBER = "member"
