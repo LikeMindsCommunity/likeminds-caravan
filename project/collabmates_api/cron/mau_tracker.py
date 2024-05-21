008cc3c1e8838fd44d09549d6586295872b64921
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2024-05-21 17:11:28 +0530

    [LM-11241] Caravan APi fixes for 500 errors (#5658)
    * fix for v2/fetch_chatroom invalid chatroom_id
    * fix for /fetch_conversation for invalid conversation_id
    * Support of GET sdk/initiate for chat sdk

--- project/collabmates_api/cron/mau_tracker.py
+++ project/collabmates_api/cron/mau_tracker.py
@@ -352,6 +352,16 @@ def create_full_text_search_coralogix_filter(api_key: str, sdk_source: str):
                             },
                             {
                                 'bool': {
+                                    'must': 
+                                    [
+                                        {
+                                            'match_phrase': { 'text': 'api/sdk/initiate?uuid' },
+                                        }
+                                    ]
+                                }
+                            },
+                            {
+                                'bool': {
                                     'must': [
                                         {
                                             'match_phrase': {
