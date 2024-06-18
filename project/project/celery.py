ee665b0897f4b1d88350ef5422b5ba2d7df23a33
ankit-grg <ankit.garg@likeminds.community>
2024-06-18 23:33:05 +0530

    Reverting the celery.py changes.

--- project/project/celery.py
+++ project/project/celery.py
@@ -9,12 +9,10 @@ load_dotenv()
 # set the default Django settings module for the 'celery' program.
 # set the default Django settings module for the 'celery' program.
 
-# if settings.IS_BETA:
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.beta')
-# else:
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.production')
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.development')
+if settings.IS_BETA:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.beta')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.production')
 
 app = Celery('project', backend='amqp', broker=os.getenv('BROKER_URL'))
 # Using a string here means the worker doesn't have to serialize
