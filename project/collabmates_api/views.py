fc482d5bf8d51d9681316dd15baa7296e63f4ae6
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-07-02 14:56:52 +0530

    add: swarm cache deletion on feedrom follow/unfollow

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -25,7 +25,8 @@ update_priority, WebhookTypes
 from utility.cache_keys import (CONVERSATION_COMMUNITY_PREVIEW, EVENT_ATTENDEES_CHATROOM, EVENT_INSTRUCTORS_CHATROOM,
                                 EVENT_HIGHLIGHTS_CHATROOM, EVENT_FAQ_CHATROOM, EVENT_MEMBERTESTIMONIALS_CHATROOM,
                                 EVENT_ATTENDEES_CONVERSATION, CHATROOM_PARTICIPANTS_CREATED_CACHE_KEY,
-                                INTERNATIONAL_OTP_GENERATE_CACHE_KEY, KETTLE_CACHE_KEY_USER_META)
+                                INTERNATIONAL_OTP_GENERATE_CACHE_KEY, KETTLE_CACHE_KEY_USER_META, 
+                                SWARM_CACHE_KEY_USER_COMMUNITY_CHANNELS)
 from utility.celery_tasks import (
     update_last_unseen_in_engage_on_card_creation,
     update_last_unseen_in_engage, update_my_chatrooms_for_users,
@@ -6448,6 +6449,14 @@ def follow_chatroom_async(collabcard_id,
                                                               users_list=[user_instance.id],
                                                               event_type=WebhookTypes.CHATROOM_JOINED.value, 
                                                               type_method=webhook_chatroom_methods.SELF_JOINED)
+
+    # If feedroom, delete cache in swarm for user community channels
+    if card_instance.type == card_types.CARD_FEED_GROUP:
+        InternalServiceUtilities.delete_cache_from_kettle_service.delay(
+                    community_id=community_instance.id, user_id=user_instance.id,
+                    key_patterns=[SWARM_CACHE_KEY_USER_COMMUNITY_CHANNELS.format(community_instance.id, 
+                                                                       user_instance.userinfo.user_unique_id)]
+                )
         
 
     send_sync_notification.delay({'chatroom_id': card_instance.id,
