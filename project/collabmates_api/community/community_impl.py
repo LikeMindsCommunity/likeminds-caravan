1d21de7c0a68c243874c21748c38eb6f19dd4a1a
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-10-15 23:39:44 +0530

    PR Changes

--- project/collabmates_api/community/community_impl.py
+++ project/collabmates_api/community/community_impl.py
@@ -6196,12 +6196,7 @@ class CommunityHelper:
                     
                 if record_updated:
                     
-                    # Delete kettle feed_metadata cache corresponding to anonymous user
-                    InternalServiceUtilities.delete_cache_from_kettle_service.delay(
-                        community_id=community_id, user_id=user_id,
-                        key_pattern=KETTLE_CACHE_KEY_FEED_META_CONFIGURATIONS.format(community_id))
-                    
-                    # Delete kettle cache corresponding to universal feed
+                    # Delete kettle cache for anonymous user
                     InternalServiceUtilities.delete_cache_from_kettle_service.delay(
                         community_id=community_id, user_id=user_id,
                         key_pattern=KETTLE_CACHE_KEY_ANONYMOUS_USER_META.format(community_id))
@@ -6425,6 +6420,12 @@ class CommunityHelper:
                     community_id=community_id, user_id=user_id,
                     key_patterns=[KETTLE_CACHE_KEY_PROFILE_META_CONFIGURATIONS.format(community_id)]
                 )
+                
+            if configuration_type in [FEED_METADATA_CONFIGURATION]:
+                # Delete kettle feed_metadata cache corresponding to anonymous user
+                InternalServiceUtilities.delete_cache_from_kettle_service.delay(
+                    community_id=community_id, user_id=user_id,
+                    key_pattern=KETTLE_CACHE_KEY_FEED_META_CONFIGURATIONS.format(community_id))
 
             # Delete cache key for widget configurations
             if configuration_type in [WIDGETS_METADATA_CONFIGURATION]:
