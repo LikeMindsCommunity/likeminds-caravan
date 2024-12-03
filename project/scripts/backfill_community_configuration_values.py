dd87e9d14fffadc7b4534ce89e226a57c45a659f
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-12-03 18:34:52 +0530

    Add backfill script for auto-approve post configurations in enabled communities

--- project/scripts/backfill_community_configuration_values.py
+++ project/scripts/backfill_community_configuration_values.py
@@ -1,9 +1,13 @@
-from togther.models import CommunityConfigurations
-from utility.constants import COMMUNITY_CONFIGURATIONS
+from togther.models import CommunityConfigurations, CommunitySettings, Members
+from utility.constants import COMMUNITY_CONFIGURATIONS, FEED_SETTINGS_CONFIGURATION
+from utility.states import community_setting_types
+from collabmates_api.community.community_impl import CommunityHelper
 
 COMMUNITY_CONFIGURATION_TYPE = ""
 
 def backfill_community_configurations():
+    
+    print("Starting the backfill_community_configurations script")
     
     if not COMMUNITY_CONFIGURATION_TYPE:
         print("Please set the COMMUNITY_CONFIGURATION_TYPE variable")
@@ -28,6 +32,34 @@ def backfill_community_configurations():
 
     print(f"Backfilled {records.count()} records for type '{COMMUNITY_CONFIGURATION_TYPE}'")
 
-print("Starting the script")
-backfill_community_configurations()
-print("Script completed")
+def backfill_auto_approve_post_configurations_for_enabled_communities():
+
+    # Fetch all the records from community settings where type = 'auto_approve_post' and enabled = True
+    records = CommunityConfigurations.objects.filter(
+        type=community_setting_types.POST_APPROVAL_NEEDED, enabled=True)
+
+    for record in records:
+
+        user_id = record.enabled_by
+
+        if not user_id:
+
+            # Fetch bot id
+            bot_id = Members.get_community_owner_user_instance_or_none(record.community_id)
+
+            if not bot_id:
+                print(f"Could not find bot id for community_id: {record.community_id}")
+                continue
+            else:
+                user_id = bot_id
+
+        update_values = {
+            "auto_approve_post": "no_one"
+        }
+
+        CommunityHelper.update_configuration_of_community(
+            community_id=record.community_id,
+            user_id=user_id,
+            configuration_type=FEED_SETTINGS_CONFIGURATION,
+            update_values=update_values
+        )
