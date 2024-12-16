117228cd8c77a6ae2543e595403216bb580399bf
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-12-16 23:18:51 +0530

    feat: add support for filtering chatrooms by custom tag in sync operations

--- project/collabmates_api/sync/sync_manager.py
+++ project/collabmates_api/sync/sync_manager.py
@@ -13,7 +13,7 @@ class SyncManager(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def sync_chatrooms(self, page: int = None, page_size: int = None, min_timestamp: int = None,
                        max_timestamp: int = None, chatroom_type: list = None, is_local_db: bool = True,
-                       included_conversation_states: list = None, chatroom_id: str = None) -> dict:
+                       included_conversation_states: list = None, chatroom_id: str = None, tag: str = None) -> dict:
         """
         Sync chatrooms data for local db
         """
