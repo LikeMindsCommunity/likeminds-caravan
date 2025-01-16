8d2a783509280f7452f188c157d79f8f7d489b71
shubh-gupta00 <shubh.gupta@likeminds.community>
2025-01-16 23:56:10 +0530

    fix: increase Gunicorn workers to 5 in entry-caravan-load.sh

--- docker-entrypoint/entry-caravan-load.sh
+++ docker-entrypoint/entry-caravan-load.sh
@@ -1,3 +1,3 @@
 source /opt/venv/bin/activate
 cd ./project
-DJANGO_SETTINGS_MODULE=project.settings.load gunicorn --workers=1 --threads=5 --bind 0.0.0.0:8081 project.wsgi
+DJANGO_SETTINGS_MODULE=project.settings.load gunicorn --workers=5 --threads=5 --bind 0.0.0.0:8081 project.wsgi
