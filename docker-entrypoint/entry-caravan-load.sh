2f4962f2abde40c32c7b4129c66a2217bebe271b
shubh-gupta00 <shubh.gupta@likeminds.community>
2025-01-25 15:16:55 +0530

    fix: update Gunicorn configuration to use 18 workers in entry-caravan-load.sh

--- docker-entrypoint/entry-caravan-load.sh
+++ docker-entrypoint/entry-caravan-load.sh
@@ -1,3 +1,3 @@
 source /opt/venv/bin/activate
 cd ./project
-DJANGO_SETTINGS_MODULE=project.settings.load gunicorn --workers=9 --threads=10 --log-level debug --bind 0.0.0.0:8081 project.wsgi
+DJANGO_SETTINGS_MODULE=project.settings.load gunicorn --workers=18 --log-level debug --bind 0.0.0.0:8081 project.wsgi
