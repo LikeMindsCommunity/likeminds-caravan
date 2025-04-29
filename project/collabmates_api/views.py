8ec42d3caeca5d4b96d0056f4cc4c71653598acb
shobhit <shobhit.singh@likeminds.community>
2025-04-30 01:53:14 +0530

    fix : removed caching from kettle and implemented in caravan for feed_member_access

--- project/collabmates_api/views.py
+++ project/collabmates_api/views.py
@@ -25,7 +25,7 @@ update_priority, WebhookTypes
 from utility.cache_keys import (CONVERSATION_COMMUNITY_PREVIEW, EVENT_ATTENDEES_CHATROOM, EVENT_INSTRUCTORS_CHATROOM,
                                 EVENT_HIGHLIGHTS_CHATROOM, EVENT_FAQ_CHATROOM, EVENT_MEMBERTESTIMONIALS_CHATROOM,
                                 EVENT_ATTENDEES_CONVERSATION, CHATROOM_PARTICIPANTS_CREATED_CACHE_KEY,
-                                INTERNATIONAL_OTP_GENERATE_CACHE_KEY, KETTLE_CACHE_KEY_USER_META, 
+                                INTERNATIONAL_OTP_GENERATE_CACHE_KEY, KETTLE_CACHE_KEY_FEED_MEMBER_ACCESS_KEY_PATTERN, KETTLE_CACHE_KEY_FEED_MEMBER_ACCESS_KEY_PREFIX, KETTLE_CACHE_KEY_USER_META, 
                                 SWARM_CACHE_KEY_USER_COMMUNITY_CHANNELS)
 from utility.celery_tasks import (
     update_last_unseen_in_engage_on_card_creation,
@@ -11512,6 +11512,12 @@ def update_community_manager_rights(request):
                 key_patterns=[cache_key] 
             )
 
+            # Call Kettle api to delete cache key for feed_member_access_{user_id}_* cache
+            InternalServiceUtilities.delete_cache_from_kettle_service.delay(
+                community_id=community_id,
+                user_id=current_user_id,
+                key_patterns=[KETTLE_CACHE_KEY_FEED_MEMBER_ACCESS_KEY_PATTERN.format(user_id)])
+
             if len(rights_added) > 0:
                 send_notification_for_right_given_to_manager.delay(user_id, community_id, list(rights_added))
 
@@ -11661,6 +11667,13 @@ def remove_community_manager(request):
 
         # Delete user meta cache in kettle service
         cache_key = KETTLE_CACHE_KEY_USER_META.format(community_instance.id, user_instance.userinfo.user_unique_id)
+
+        # Call Kettle api to delete cache key for feed_member_access_{user_id}_* cache
+        InternalServiceUtilities.delete_cache_from_kettle_service.delay(
+            community_id=community_id,
+            user_id=current_user_id,
+            key_patterns=[KETTLE_CACHE_KEY_FEED_MEMBER_ACCESS_KEY_PATTERN.format(user_id)]
+        )
 
         InternalServiceUtilities.delete_cache_from_kettle_service.delay(
             community_id=community_instance.id, user_id=user_instance.id,
@@ -12065,6 +12078,13 @@ def update_community_member_rights(request):
 
         # Update chatroom indexing
         ElasticSearchSync.update_all_community_chatrooms_for_a_user.delay(user_id, community_id)
+
+        # Call Kettle api to delete cache key for feed_member_access_{user_id}_* cache
+        InternalServiceUtilities.delete_cache_from_kettle_service.delay(
+            community_id, 
+            current_user_id,
+            [KETTLE_CACHE_KEY_FEED_MEMBER_ACCESS_KEY_PATTERN.format(user_id)]
+        )
 
         return JsonResponse({'success': True})
     else:
@@ -12864,6 +12884,13 @@ def update_community_rights(request):
             f"community id = {community_id}")
 
         update_member_rights_list_for_community_members.delay(community_id)
+
+        # Call Kettle api to delete cache key for feed_member_access_* cache
+        InternalServiceUtilities.delete_cache_from_kettle_service.delay(
+                    community_id=community_id, 
+                    user_id=current_user_id,
+                    key_patterns=[KETTLE_CACHE_KEY_FEED_MEMBER_ACCESS_KEY_PREFIX]
+                )
 
         return JsonResponse({'success': True})
     else:
