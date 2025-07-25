4776737c80d2cfa074bd462a7b6f2a02e87f1232
Ketan Yadav <ketan@likeminds.community>
2025-07-25 11:40:09 +0530

    Reduce worker count to 2

--- docker-entrypoint/entry-caravan-prod.sh
+++ docker-entrypoint/entry-caravan-prod.sh
@@ -1,4 +1,4 @@
 source /opt/venv/bin/activate
 cd ./project
 export DJANGO_SETTINGS_MODULE=project.settings.production
-exec gunicorn --workers=3 --timeout=30 --bind 0.0.0.0:8081 --log-level debug project.wsgi
+exec gunicorn --workers=2 --timeout=30 --bind 0.0.0.0:8081 --log-level debug project.wsgi
