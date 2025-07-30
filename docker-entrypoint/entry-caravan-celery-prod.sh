cd4c6ec0dd4f5b41693f8ee2926766663ece24fa
Ketan Yadav <ketan@likeminds.community>
2025-07-30 18:13:33 +0530

    Update concurrency=6 for caravan-celery-es-prod

--- docker-entrypoint/entry-caravan-celery-prod.sh
+++ docker-entrypoint/entry-caravan-celery-prod.sh
@@ -3,5 +3,5 @@ touch /var/log/celery/celery.log
 touch /var/log/celery/celery_beat.log
 source /opt/venv/bin/activate
 cd ./project
-DJANGO_SETTINGS_MODULE=project.settings.production celery -A project worker --loglevel=info -Q celery --concurrency=6
+DJANGO_SETTINGS_MODULE=project.settings.production celery -A project worker -c 6 --loglevel=info -Q celery
 DJANGO_SETTINGS_MODULE=project.settings.production celery -A project beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler -Q celery
