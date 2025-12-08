e51f354da72a77270d1519bd1925dd0acb14c83c
ankit-grg <ankit.garg@likeminds.community>
2025-12-08 13:35:23 +0530

    Added concurrency in celery for beta and prod.

--- docker-entrypoint/entry-caravan-celery-beta.sh
+++ docker-entrypoint/entry-caravan-celery-beta.sh
@@ -3,5 +3,5 @@ touch /var/log/celery/celery.log
 touch /var/log/celery/celery_beat.log
 source /opt/venv/bin/activate
 cd ./project
-DJANGO_SETTINGS_MODULE=project.settings.beta celery -A project worker --loglevel=info -Q celery
+DJANGO_SETTINGS_MODULE=project.settings.beta celery -A project worker --loglevel=info -Q celery -c 4
 DJANGO_SETTINGS_MODULE=project.settings.beta celery -A project beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler -Q celery
