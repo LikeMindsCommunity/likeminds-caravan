49f2a0713253993fe43e6258d0fc79a4b138f891
Ketan Yadav <ketan@likeminds.community>
2025-12-09 20:16:39 +0530

    Add celery start params to remove cluster mode

--- docker-entrypoint/entry-caravan-celery-prod.sh
+++ docker-entrypoint/entry-caravan-celery-prod.sh
@@ -3,5 +3,5 @@ touch /var/log/celery/celery.log
 touch /var/log/celery/celery_beat.log
 source /opt/venv/bin/activate
 cd ./project
-DJANGO_SETTINGS_MODULE=project.settings.production celery -A project worker --loglevel=info -Q celery -c 4
+DJANGO_SETTINGS_MODULE=project.settings.production celery -A project worker --loglevel=info -Q celery -c 4 --without-mingle --without-gossip --without-heartbeat --prefetch-multiplier=1
 DJANGO_SETTINGS_MODULE=project.settings.production celery -A project beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler -Q celery
